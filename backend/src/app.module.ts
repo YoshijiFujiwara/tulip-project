41503a8f84faf7beddeb838cb93a791be5da8053
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-10-23 10:49:33 +0900

    change: exhibitor.module系を一旦削除...

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,13 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { typeOrmConfig } from './config/typeorm.config';
-import { ExhibitorModule } from './exhibitor/exhibitor.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
-    ExhibitorModule
-  ],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/exhibitor/exhibitor.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ExhibitorController } from './exhibitor.controller';
-import { ExhibitorService } from './exhibitor.service';
-
-@Module({
-  controllers: [ExhibitorController],
-  providers: [ExhibitorService]
-})
-export class ExhibitorModule {}
