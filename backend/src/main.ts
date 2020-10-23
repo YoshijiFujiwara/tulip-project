17bceb44988f9ded033502b176de48b89a19c2d9
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-10-23 11:19:06 +0900

    update: SwaggerModuleをmain.tsで読み込むようにした(openapi対応)

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('API description')
+    .setVersion('1.0')
+    .addServer(`http://localhost:${process.env.BACKEND_INNER_PORT}/`)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.BACKEND_INNER_PORT as string);
 }
 bootstrap();
