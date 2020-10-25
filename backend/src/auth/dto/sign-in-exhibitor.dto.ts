829118331697c53da8157492bcd63f05e1fa4a95
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-10-25 19:46:08 +0900

    add: dtoにapiProperty追加

--- backend/src/auth/dto/sign-in-exhibitor.dto.ts
+++ backend/src/auth/dto/sign-in-exhibitor.dto.ts
@@ -1,12 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class SignInExhibitorDto {
+  @ApiProperty({
+    example: 'ohs12345',
+    type: 'string',
+    minLength: 8,
+    maxLength: 8,
+  })
   @IsString()
   @MinLength(8)
   @MaxLength(8)
   @IsNotEmpty({ message: '学籍番号の入力は必須です。' })
   studentNumebr: string;
 
+  @ApiProperty({
+    example: 'hogeho',
+    type: 'string',
+    minLength: 6,
+    maxLength: 20,
+  })
   @IsString()
   @MinLength(6)
   @MaxLength(20)
