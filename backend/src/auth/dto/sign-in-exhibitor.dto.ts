cb3a22f88d8c47f45123fd595a120db90296f3da
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-10-25 19:40:28 +0900

    update: dtoのstudentNumberのmin/maxLength制限有効化

--- backend/src/auth/dto/sign-in-exhibitor.dto.ts
+++ backend/src/auth/dto/sign-in-exhibitor.dto.ts
@@ -2,8 +2,8 @@ import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class SignInExhibitorDto {
   @IsString()
-  // @MinLength(8)
-  // @MaxLength(8)
+  @MinLength(8)
+  @MaxLength(8)
   @IsNotEmpty({ message: '学籍番号の入力は必須です。' })
   studentNumebr: string;
 
