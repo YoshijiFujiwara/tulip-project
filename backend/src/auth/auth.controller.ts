8b06d8ceabf646fb21a6d80cdf22d132a53c40d5
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-10-25 20:11:39 +0900

    add: ApiOkResとか追加

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,17 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { SignInExhibitorDto } from './dto/sign-in-exhibitor.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/sign_in')
+  @ApiOkResponse({
+    description: '出展者ログイン完了',
+  })
   signIn(@Body(ValidationPipe) signInExhibitorDto: SignInExhibitorDto) {
     return this.authService.signIn(signInExhibitorDto);
   }
--- backend/src/auth/serializer/exhibitor.serializer.ts
+++ backend/src/auth/serializer/exhibitor.serializer.ts
@@ -1,9 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 export class ExhibitorSerializer {
+  @ApiProperty()
   id: string;
 
+  @ApiProperty()
   studentNumebr: string;
 
+  @ApiProperty()
   name: string;
 
+  @ApiProperty()
   lastLoggedinAt: Date;
 }
