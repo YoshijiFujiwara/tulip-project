bdb6974000468d1cae831ae8fce4b8730faf5830
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-10-26 22:04:32 +0900

    add: provide JwtStrategy

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ExhibitorRepository } from './exhibitor.repository';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   imports: [
@@ -14,6 +15,6 @@ import { JwtModule } from '@nestjs/jwt';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
