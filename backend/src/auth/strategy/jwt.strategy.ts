36992c21812745e776f325522ac680b9191a7445
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-10-26 21:46:06 +0900

    add: jwt strategy

---
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Strategy } from 'passport';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExhibitorRepository } from '../exhibitor.repository';
+import { ExtractJwt } from 'passport-jwt';
+import { JwtPayload } from '../interface/jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @InjectRepository(ExhibitorRepository)
+    private exhibitorRepository: ExhibitorRepository,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate({ studentNumber }: JwtPayload) {
+    const exhibitor = await this.exhibitorRepository.findOne({ studentNumber });
+    if (!exhibitor) {
+      throw new UnauthorizedException();
+    }
+    return exhibitor;
+  }
+}
