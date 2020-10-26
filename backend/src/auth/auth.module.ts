bbfcc4d4a4cb7aae5381ef8d8cc0646b4eb107e6
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-10-26 21:45:57 +0900

    add: inject JwtModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,9 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { ExhibitorRepository } from './exhibitor.repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ExhibitorRepository])],
+  imports: [
+    TypeOrmModule.forFeature([ExhibitorRepository]),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
