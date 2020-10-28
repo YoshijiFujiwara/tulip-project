2eb57b85bf4342be5d6b876f06cc8971ab60fcd5
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-10-29 02:15:31 +0900

    update: 日本人名でseedingできるようにした

--- backend/src/migrations/factories/exhibitor.factory.ts
+++ backend/src/migrations/factories/exhibitor.factory.ts
@@ -1,23 +1,23 @@
 import { define } from 'typeorm-seeding';
 import * as Faker from 'faker/locale/ja';
-import { ExhibitorEntity } from '../../entities/exhibitor';
+import { ExhibitorEntity } from '../../entities/exhibitor.entity';
 import { v4 as uuid } from 'uuid';
+import { randomJapaneseFullName } from '../../func/random.func';
 
-define(ExhibitorEntity, (faker: typeof Faker) => {
+interface Context {
+  id: number;
+}
+
+define(ExhibitorEntity, (_: typeof Faker, context: Context) => {
+  const { id } = context;
+
   const exhibitor = new ExhibitorEntity();
-
   exhibitor.id = uuid();
-  exhibitor.studentNumber = getRandomStudentNumber();
+  exhibitor.studentNumber = `ohs7${('0000' + id).slice(-4)}`;
   exhibitor.password = 'B19990101';
-  exhibitor.name = `${faker.name.lastName} ${faker.name.firstName}`;
+
+  exhibitor.name = `${randomJapaneseFullName()}`;
   exhibitor.lastLoggedinAt = new Date();
 
   return exhibitor;
 });
-
-function getRandomStudentNumber() {
-  const min = 10000;
-  const max = 99999;
-  const number = Math.random() * (max - min) + min;
-  return `ohs${number}`;
-}
--- backend/src/migrations/seeds/create-exhibitors.seed.ts
+++ backend/src/migrations/seeds/create-exhibitors.seed.ts
@@ -1,8 +1,17 @@
+import { getRepository } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
-import { ExhibitorEntity } from '../../entities/exhibitor';
+import { ExhibitorEntity } from '../../entities/exhibitor.entity';
 
 export default class CreateExhibitors implements Seeder {
   public async run(factory: Factory) {
-    await factory(ExhibitorEntity)().createMany(10);
+    const exhibitorRepository = getRepository(ExhibitorEntity);
+    const maxCnt = await exhibitorRepository.count();
+
+    await Promise.all(
+      [...Array(9)].map(async (_: any, id: number) => {
+        const insertId = id + maxCnt;
+        return await factory(ExhibitorEntity)({ id: insertId }).create();
+      }),
+    );
   }
 }
