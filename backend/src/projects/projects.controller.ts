2aee68da198dac8bb22c351c469b0d09f4096fe1
ohs70457 <ayuka.70457@gmail.com>
2020-10-30 13:03:38 +0900

    add: 　Postでhogehogeを表示

--- backend/src/projects/projects.controller.ts
+++ backend/src/projects/projects.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import { ProjectsService } from './projects.service';
 
+@ApiTags('projects')
 @Controller('projects')
-export class ProjectsController {}
+// @UseGuards(AuthGuard('jwt'))
+export class ProjectsController {
+  constructor(private projectsService: ProjectsService) {}
+
+  @Post()
+  @ApiCreatedResponse({
+    type: 'string',
+    description: '作品登録完了',
+  })
+  async createProject(): Promise<string> {
+    return 'hogehoge';
+  }
+}
