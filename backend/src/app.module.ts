f9f2e546afe065191ec03c66c333ec0609e6ebee
ohs70457 <ayuka.70457@gmail.com>
2020-10-30 12:08:50 +0900

    add: projects_module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), AuthModule],
+  imports: [TypeOrmModule.forRoot(), AuthModule, ProjectsModule],
   controllers: [AppController],
   providers: [AppService],
 })
