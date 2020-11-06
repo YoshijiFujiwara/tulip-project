7a84f40f05c06c22db3d544a7732deb2fc391819
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-11-06 12:22:04 +0900

    update: migrations

---
+++ backend/src/migrations/seeds/1603424284188-create-groups.seed.ts
@@ -0,0 +1,7 @@
+import { Factory, Seeder } from 'typeorm-seeding';
+
+export default class CreateGroups implements Seeder {
+  public async run(factory: Factory) {
+    // const groupsRepsository =;
+  }
+}
