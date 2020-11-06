5e1c425fb5f05ddeb4dca5eab483f733fae4b746
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-11-06 14:38:00 +0900

    update: groupsテーブルにtimestamp追加

--- backend/src/entities/group.entity.ts
+++ backend/src/entities/group.entity.ts
@@ -4,6 +4,8 @@ import {
   PrimaryGeneratedColumn,
   Column,
   OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { ExhibitorEntity } from './exhibitor.entity';
@@ -27,4 +29,19 @@ export class GroupEntity extends BaseEntity {
     exhibitor => exhibitor.group,
   )
   exhibitors: ExhibitorEntity[];
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  @ApiProperty()
+  createdAt!: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  @ApiProperty()
+  updatedAt!: Date;
 }
