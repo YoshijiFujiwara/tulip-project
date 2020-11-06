e119302dbee0e7c46b030810080fc1d7606b1268
kooririri <piaoxianglunb@gmail.com>
2020-11-06 11:56:17 +0900

    cssでフォームを真ん中にする

--- frontend/pages/auth/signin.vue
+++ frontend/pages/auth/signin.vue
@@ -7,50 +7,51 @@
     >
       <v-container class="grey lighten-5">
         <v-img max-width="1114px" :src="require('@/assets/logo_main.png')" />
-        <v-row align="center" justify-center no-gutters cols="12">
-          <v-col cols="6" align="center" class="title" offset="6">
-            <span class="title px-10">SIGN IN</span>
+        <v-row id="signin-wrap" justify="center" class="row-wrap" no-gutters>
+          <v-col cols="12" align="center" offset="2" class="title">
+            <span class="SIGN-IN">SIGN IN</span>
           </v-col>
         </v-row>
-        <!-- <v-row align="center" justify="center" class="md-1" no-gutters> -->
-        <v-col offset-md="4" md="6" class="top" align="center">
-          <v-form ref="form">
-            <v-col cols="6">
-              <v-text-field
-                v-model="form.studentNumber"
-                background-color="#281252"
-                dark
-                class="mt-10"
-                label="学籍番号"
-                outlined
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="6">
-              <v-text-field
-                v-model="form.password"
-                background-color="#281252"
-                dark
-                class="mt-7"
-                label="パスワード"
-                outlined
-                required
-              ></v-text-field>
-            </v-col>
-            <v-col cols="6">
-              <v-btn
-                block
-                large
-                dark
-                color="#27144e"
-                class="signin-btn ma-5"
-                @click="onSubmit"
-                >Log in</v-btn
-              >
-            </v-col>
-          </v-form>
-        </v-col>
-        <!-- </v-row> -->
+        <v-row align="center" justify="center" class="row-wrap" no-gutters>
+          <v-col md="12" class="top" align="center">
+            <v-form ref="form">
+              <v-col cols="8">
+                <v-text-field
+                  v-model="form.studentNumber"
+                  background-color="#281252"
+                  dark
+                  prepend-inner-icon="mdi-account"
+                  class="mt-10"
+                  label="学籍番号"
+                  outlined
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="8">
+                <v-text-field
+                  v-model="form.password"
+                  prepend-inner-icon="mdi-lock"
+                  background-color="#281252"
+                  dark
+                  label="パスワード"
+                  outlined
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="8">
+                <v-btn
+                  block
+                  large
+                  dark
+                  color="#27144e"
+                  class="signin-btn ma-5"
+                  @click="onSubmit"
+                  >Log in</v-btn
+                >
+              </v-col>
+            </v-form>
+          </v-col>
+        </v-row>
       </v-container>
     </v-img>
   </v-row>
@@ -91,14 +92,46 @@ export default Vue.extend({
 
 <style>
 .title {
-  background-color: #5f439a;
+  background-color: rgba(255, 255, 255, 0.18);
   font-size: 3rem;
-  color: #cbbee7;
+}
+.row-wrap {
+  max-width: 600px;
+  margin: 0 auto;
+  background-color: rgba(20, 8, 44, 0.59);
 }
 .top {
   background-color: #14082c;
 }
 .signin-btn {
-  border: solid 3px #5f439a;
+  border: solid 3px rgba(255, 255, 255, 0.37);
+
+  background-color: rgba(255, 255, 255, 0.23);
+}
+.SIGN-IN {
+  width: 228px;
+
+  height: 66px;
+
+  font-family: MeiryoUI;
+
+  font-size: 52px;
+
+  font-weight: bold;
+
+  font-stretch: normal;
+
+  font-style: normal;
+
+  line-height: 0.69;
+
+  letter-spacing: normal;
+
+  text-align: center;
+
+  color: rgba(255, 255, 255, 0.76);
+}
+#signin-wrap {
+  margin-top: 50px;
 }
 </style>
