ca851ed59acb85b88e4add5875972a7735995050
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-11-06 12:55:52 +0900

    update: group seeder作成 & exhibitor seeder更新

--- backend/src/migrations/seeds/1604548191769-create-exhibitors.seed.ts
+++ backend/src/migrations/seeds/1604548191769-create-exhibitors.seed.ts
@@ -1,16 +1,25 @@
 import { getRepository } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
 import { ExhibitorEntity } from '../../entities/exhibitor.entity';
+import { GroupEntity } from '../../entities/group.entity';
 
 export default class CreateExhibitors implements Seeder {
   public async run(factory: Factory) {
     const exhibitorRepository = getRepository(ExhibitorEntity);
-    const maxCnt = (await exhibitorRepository.count()) + 1;
+    const exhibitorMaxCnt = (await exhibitorRepository.count()) + 1;
+
+    const groupRepsository = getRepository(GroupEntity);
+    const groupMaxCnt = await groupRepsository.count();
 
     await Promise.all(
       [...Array(9)].map(async (_: any, id: number) => {
-        const insertId = id + maxCnt;
-        return await factory(ExhibitorEntity)({ id: insertId }).create();
+        const insertId = id + exhibitorMaxCnt;
+        const groupId = Math.floor(Math.random() * groupMaxCnt) + 1;
+
+        return await factory(ExhibitorEntity)({
+          id: insertId,
+          groupId,
+        }).create();
       }),
     );
   }
