59d413133ebe70fce65ec4500166c3bd7c9a4b10
YoshijiFujiwara <yfbiology2@gmail.com>
2020-11-07 13:55:01 +0900

    document.getElementById('parallax-full-height')周りのtsの警告対応

--- frontend/pages/auth/signin.vue
+++ frontend/pages/auth/signin.vue
@@ -79,7 +79,9 @@ export default Vue.extend({
   },
   mounted() {
     // parallaxコンポーネントを無理やり、高さを合わせる（汚いやり方だけど、仕方ない）
-    document.getElementById('parallax-full-height').style.height = '100vh'
+    if (document.getElementById('parallax-full-height')) {
+      document.getElementById('parallax-full-height').style.height = '100vh'
+    }
   },
   methods: {
     async onSubmit() {
