0a67aad289243e5a8b2b7c914e4cc4cf7c60f946
YoshijiFujiwara <yfbiology2@gmail.com>
2020-11-09 12:36:39 +0900

    マイページで、空の作品登録モーダルを開くように

---
+++ frontend/components/exhibitors/mypage/CreateExhibitDialog.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-dialog v-model="dialog" max-width="600px">
+    <v-card> hogehoge </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class CreateExhibitDialog extends Vue {
+  // モーダルの開閉のために必要なprops
+  @Prop({ required: true }) value: boolean = false
+
+  // モーダルの開閉のために必要
+  get dialog(): boolean {
+    return this.value
+  }
+
+  // モーダルの開閉のために必要
+  set dialog(dialog: boolean) {
+    this.$emit('input', dialog)
+  }
+}
+</script>
