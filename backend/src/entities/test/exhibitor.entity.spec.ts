49370d5ec06e8925c6f03faf112094d287c15c47
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-11-10 13:12:56 +0900

    add: exhibitor.entity.spec

---
+++ backend/src/entities/test/exhibitor.entity.spec.ts
@@ -0,0 +1,37 @@
+import { ExhibitorEntity } from '../exhibitor.entity';
+
+describe('ExhibitorEntity', () => {
+  let exhibitor: ExhibitorEntity;
+
+  beforeEach(() => {
+    exhibitor = new ExhibitorEntity();
+    exhibitor.id = 1;
+    exhibitor.studentNumber = 'ohs70001';
+    exhibitor.password = 'B19990101';
+    exhibitor.name = '田中太郎';
+    exhibitor.groupId = 1;
+
+    const date = new Date();
+    exhibitor.lastLoggedinAt = date;
+    exhibitor.createdAt = date;
+    exhibitor.updatedAt = date;
+  });
+
+  describe('transformToSerializer', () => {
+    it('シリアライズされるパラメータが正しいか', () => {
+      const result = exhibitor.transformToSerializer();
+      expect(result.id).toEqual(exhibitor.id);
+      expect(result.studentNumber).toEqual(exhibitor.studentNumber);
+      expect(result.name).toEqual(exhibitor.name);
+      expect(result.groupId).toEqual(exhibitor.groupId);
+      expect(result.lastLoggedinAt).toEqual(exhibitor.lastLoggedinAt);
+
+      expect(hasProperty(result, 'password')).toEqual(false);
+      expect(hasProperty(result, 'createdAt')).toEqual(false);
+      expect(hasProperty(result, 'updatedAt')).toEqual(false);
+    });
+  });
+});
+
+const hasProperty = (obj, key) =>
+  !!obj && Object.prototype.hasOwnProperty.call(obj, key);
