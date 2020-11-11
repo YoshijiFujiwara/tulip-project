368750b5dc10aef04d63ff139e222ccc9d7092d8
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-11-11 12:25:06 +0900

    add: exhibit.entity.spec

---
+++ backend/src/entities/test/exhibit.entity.spec.ts
@@ -0,0 +1,70 @@
+import { ExhibitEntity, GENRE } from '../exhibit.entity';
+import { ExhibitorEntity } from '../exhibitor.entity';
+import { GroupEntity } from '../group.entity';
+import { hasProperty } from '../../func/test.func';
+
+describe('ExhibitEntity', () => {
+  let exhibit: ExhibitEntity;
+  let mockExhibitors: ExhibitorEntity[];
+  let mockGroup: GroupEntity;
+
+  beforeEach(() => {
+    mockExhibitors = [];
+
+    mockGroup = new GroupEntity();
+    mockGroup.id = 1;
+    mockGroup.name = 'チームA';
+    mockGroup.createdAt = new Date();
+    mockGroup.updatedAt = new Date();
+
+    ['山田花子', '佐藤二朗'].map((name, id) => {
+      const mockExhibitor = new ExhibitorEntity();
+      mockExhibitor.id = id + 1;
+      mockExhibitor.studentNumber = `ohs7000${mockExhibitor.id}`;
+      mockExhibitor.password = 'B19990101';
+      mockExhibitor.name = name;
+      mockExhibitor.groupId = mockGroup.id;
+
+      const addUserDate = new Date();
+      mockExhibitor.lastLoggedinAt = addUserDate;
+      mockExhibitor.createdAt = addUserDate;
+      mockExhibitor.updatedAt = addUserDate;
+      mockExhibitors = [...mockExhibitors, mockExhibitor];
+    });
+    mockGroup.exhibitors = mockExhibitors;
+
+    exhibit = new ExhibitEntity();
+    exhibit.id = mockGroup.id;
+    exhibit.title = '作品A';
+    exhibit.description = 'hogehoge';
+    exhibit.thumbnail = 'hogehoge';
+    exhibit.genre = GENRE.IT;
+    exhibit.presentationImage = 'hogehoge';
+    exhibit.group = mockGroup;
+    exhibit.groupId = mockGroup.id;
+
+    const date = new Date();
+    exhibit.createdAt = date;
+    exhibit.updatedAt = date;
+  });
+
+  describe('transformToSerializer', () => {
+    it('シリアライズされるパラメータが正しいか', () => {
+      const result = exhibit.transformToSerializer();
+      expect(result.id).toEqual(exhibit.id);
+      expect(result.title).toEqual(exhibit.title);
+      expect(result.description).toEqual(exhibit.description);
+      expect(result.thumbnail).toEqual(exhibit.thumbnail);
+      expect(result.genre).toEqual(exhibit.genre);
+      expect(result.presentationImage).toEqual(exhibit.presentationImage);
+      expect(result.groupId).toEqual(exhibit.groupId);
+      expect(result.group).toEqual(mockGroup.transformToSerializer());
+      expect(result.group.exhibitors).toEqual(
+        mockExhibitors.map(e => e.transformToSerializer()),
+      );
+
+      expect(hasProperty(result, 'createdAt')).toEqual(false);
+      expect(hasProperty(result, 'updatedAt')).toEqual(false);
+    });
+  });
+});
