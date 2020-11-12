df96d87991d4fdd4d1bd5f7716c26aa6109285f5
kooririri <piaoxianglunb@gmail.com>
2020-11-12 15:24:32 +0900

    作品一覧取得本番API

--- backend/src/exhibits/exhibits.controller.ts
+++ backend/src/exhibits/exhibits.controller.ts
@@ -77,26 +77,8 @@ export class ExhibitsController {
     type: [ExhibitSerializer],
     description: '作品情報一覧を配列で取得',
   })
-  getAllExhibits() {
-    const exhibit1 = new ExhibitEntity();
-    exhibit1.title = '作品名1やで';
-    exhibit1.description = 'hogehogs1';
-    exhibit1.thumbnail =
-      'https://i.gzn.jp/img/2018/01/15/google-gorilla-ban/00.jpg';
-    exhibit1.presentationImage =
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwired.jp%2F2018%2F01%2F18%2Fgorillas-and-google-photos%2F&psig=AOvVaw0q-C6ITVrxJwXa3kbTHooK&ust=1605000065833000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKDd6aiR9ewCFQAAAAAdAAAAABAD';
-    exhibit1.genre = GENRE.IT;
-    exhibit1.groupId = 1;
-
-    const exhibit2 = new ExhibitEntity();
-    exhibit2.title = '作品名2やで';
-    exhibit2.description = 'hogehogs2';
-    exhibit2.thumbnail =
-      'https://i.gzn.jp/img/2018/01/15/google-gorilla-ban/00.jpg';
-    exhibit2.presentationImage =
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwired.jp%2F2018%2F01%2F18%2Fgorillas-and-google-photos%2F&psig=AOvVaw0q-C6ITVrxJwXa3kbTHooK&ust=1605000065833000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKDd6aiR9ewCFQAAAAAdAAAAABAD';
-    exhibit2.genre = GENRE.GAME;
-    exhibit2.groupId = 2;
-    return [exhibit1, exhibit2];
+  async getExhibits(): Promise<ExhibitSerializer[]> {
+    const exhibits = await this.exhibitsService.getExhibits();
+    return exhibits.map(e => e.transformToSerializer());
   }
 }
