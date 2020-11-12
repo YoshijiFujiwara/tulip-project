3d11cda3196f5b4397ac7a9e9ae7c642323866f4
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-11-12 22:03:59 +0900

    add: getExhibit()

--- backend/src/exhibits/exhibits.controller.ts
+++ backend/src/exhibits/exhibits.controller.ts
@@ -3,6 +3,8 @@ import {
   Controller,
   Get,
   HttpCode,
+  Param,
+  ParseIntPipe,
   Post,
   Put,
   UseGuards,
@@ -98,5 +100,29 @@ export class ExhibitsController {
     exhibit2.genre = GENRE.GAME;
     exhibit2.groupId = 2;
     return [exhibit1, exhibit2];
+  }
+
+  @Get(':id')
+  @ApiOkResponse({
+    type: ExhibitSerializer,
+    description: '作品情報を取得',
+  })
+  async getExhibit(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<ExhibitSerializer> {
+    const exhibit = new ExhibitEntity();
+    exhibit.id = id;
+    exhibit.title = '作品名1やで';
+    exhibit.description = 'hogehogs1';
+    exhibit.thumbnail =
+      'https://i.gzn.jp/img/2018/01/15/google-gorilla-ban/00.jpg';
+    exhibit.presentationImage =
+      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwired.jp%2F2018%2F01%2F18%2Fgorillas-and-google-photos%2F&psig=AOvVaw0q-C6ITVrxJwXa3kbTHooK&ust=1605000065833000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKDd6aiR9ewCFQAAAAAdAAAAABAD';
+    exhibit.genre = GENRE.IT;
+    exhibit.groupId = id;
+    exhibit.createdAt = new Date();
+    exhibit.updatedAt = new Date();
+
+    return await exhibit.transformToSerializer();
   }
 }
