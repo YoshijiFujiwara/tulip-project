fb9651e126e7e830a08dfafda30c247e0690852d
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-11-13 11:00:37 +0900

    add: exhibit.service にmockExhibitorを追加

--- backend/src/exhibits/exhibits.service.spec.ts
+++ backend/src/exhibits/exhibits.service.spec.ts
@@ -2,11 +2,21 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ExhibitsService } from './exhibits.service';
 import { ExhibitRepsitory } from '../entities/exhibit.repository';
 import { GroupRepository } from '../entities/group.repository';
+import { ExhibitorEntity } from '../entities/exhibitor.entity';
 
 const mockExhibitRepository = () => ({
   createExhibit: jest.fn(),
 });
 const mockGroupRepository = () => ({});
+
+const mockExhibitor = new ExhibitorEntity();
+mockExhibitor.id = 1;
+mockExhibitor.studentNumber = 'ohs70001';
+mockExhibitor.password = 'B19990101';
+mockExhibitor.name = '山田太郎';
+mockExhibitor.lastLoggedinAt = new Date();
+mockExhibitor.createdAt = new Date();
+mockExhibitor.updatedAt = new Date();
 
 describe('ExhibitsService', () => {
   let exhibitsService;
