99d541c17f404dde89dddfe35b667e2e49cf9efd
ohs70457 <ayuka.70457@gmail.com>
2020-11-19 22:13:10 +0900

    add:自分の作品を取得するAPI（本番）

--- backend/src/profile/profile.controller.ts
+++ backend/src/profile/profile.controller.ts
@@ -1,7 +1,15 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiNoContentResponse,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
-import { GENRE } from 'src/entities/exhibit.entity';
+import { GetUser } from '../auth/get-user-decorator';
+import { ExhibitorEntity } from '../entities/exhibitor.entity';
+import { ExhibitSerializer } from '../entities/serializer/exhibit.serializer';
+import { ProfileService } from './profile.service';
 
 /**
  * 自分の登録してる作品情報やブース情報を扱うcontroller
@@ -12,22 +20,21 @@ import { GENRE } from 'src/entities/exhibit.entity';
 @UseGuards(AuthGuard('jwt'))
 @ApiBearerAuth()
 export class ProfileController {
+  constructor(private profileService: ProfileService) {}
+
   @Get('exhibit')
-  async getExhibit() {
-    return {
-      id: 1,
-      title: '作品1',
-      description: 'hogehoge',
-      thumbnail:
-        'https://res.cloudinary.com/db32y726v/image/upload/v1596079557/vjzwcimeqkmtwj6fugbj.jpg',
-      genre: GENRE.MUSIC,
-      presentationImage:
-        'https://res.cloudinary.com/db32y726v/image/upload/v1596079557/vjzwcimeqkmtwj6fugbj.jpg',
-      groupId: 1,
-      group: {
-        id: 1,
-        name: 'チーム1',
-      },
-    };
+  @ApiResponse({
+    status: 200,
+    type: [ExhibitSerializer],
+    description: '自身の作品情報を取得',
+  })
+  @ApiNoContentResponse({
+    description: '作品情報が登録されていません',
+  })
+  async getExhibit(
+    @GetUser() exhibitor: ExhibitorEntity,
+  ): Promise<ExhibitSerializer> {
+    const exhibit = await this.profileService.getExhibit(exhibitor.groupId);
+    return exhibit.transformToSerializer();
   }
 }
--- backend/src/profile/profile.service.ts
+++ backend/src/profile/profile.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ExhibitRepsitory } from '../entities/exhibit.repository';
+import { GroupRepository } from '../entities/group.repository';
+import { ExhibitEntity } from '../entities/exhibit.entity';
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+  constructor(
+    @InjectRepository(ExhibitRepsitory)
+    private exhibitRepository: ExhibitRepsitory,
+    @InjectRepository(GroupRepository)
+    private groupRepository: GroupRepository,
+  ) {}
+
+  async getExhibit(groupId: number): Promise<ExhibitEntity> {
+    const exhibit = await this.exhibitRepository.findOne({
+      relations: ['group'],
+      where: { id: groupId },
+    });
+    if (!exhibit) {
+      throw new NotFoundException('該当する作品が存在しません');
+    }
+    return exhibit;
+  }
+}
