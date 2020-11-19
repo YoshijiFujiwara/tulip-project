1b90aa18d181d0c849692182037827d76645322a
YoshijiFujiwara <yfbiology2@gmail.com>
2020-11-19 11:17:59 +0900

    自分の作品情報を取得するダミーAPIを作成

---
+++ backend/src/profile/profile.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { GENRE } from 'src/entities/exhibit.entity';
+
+/**
+ * 自分の登録してる作品情報やブース情報を扱うcontroller
+ * 全てのルートで認証必須
+ */
+@ApiTags('exhibits')
+@Controller('profile')
+@UseGuards(AuthGuard('jwt'))
+@ApiBearerAuth()
+export class ProfileController {
+  async getExhibit() {
+    return {
+      id: 1,
+      title: '作品1',
+      description: 'hogehoge',
+      thumbnail:
+        'https://res.cloudinary.com/db32y726v/image/upload/v1596079557/vjzwcimeqkmtwj6fugbj.jpg',
+      genre: GENRE.MUSIC,
+      presentationImage:
+        'https://res.cloudinary.com/db32y726v/image/upload/v1596079557/vjzwcimeqkmtwj6fugbj.jpg',
+      groupId: 1,
+      group: {
+        id: 1,
+        name: 'チーム1',
+      },
+    };
+  }
+}
