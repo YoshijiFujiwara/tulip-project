10956beb586129ea557cec2b51a0506769e71748
YoshijiFujiwara <yfbiology2@gmail.com>
2020-11-20 01:11:10 +0900

    色修正

--- frontend/pages/vr/shop.vue
+++ frontend/pages/vr/shop.vue
@@ -1,22 +1,20 @@
 <template>
-  <div>
-    <a-scene>
-      <a-assets timeout="200000">
-        <a-asset-item id="shop-obj" src="/vr/model/shop.obj"></a-asset-item>
-        <a-asset-item id="shop-mtl" src="/vr/model/shop.mtl"></a-asset-item>
-      </a-assets>
-      <a-obj-model
-        v-for="(n, i) in 20"
-        :key="i"
-        :position="`${i * 10} -1 ${i % 2 == 0 ? 20 : 0}`"
-        :rotation="`-90 0 ${i % 2 == 0 ? 180 : 0}`"
-        src="#shop-obj"
-        mtl="#shop-mtl"
-        scale="0.05 0.05 0.05"
-      ></a-obj-model>
-      <a-sky color="#000"></a-sky>
-    </a-scene>
-  </div>
+  <a-scene>
+    <a-assets timeout="200000">
+      <a-asset-item id="shop-obj" src="/vr/model/shop.obj"></a-asset-item>
+      <a-asset-item id="shop-mtl" src="/vr/model/shop.mtl"></a-asset-item>
+    </a-assets>
+    <a-obj-model
+      v-for="(n, i) in 20"
+      :key="i"
+      :position="`${i * 10} -1 ${i % 2 == 0 ? 20 : 0}`"
+      :rotation="`-90 0 ${i % 2 == 0 ? 180 : 0}`"
+      src="#shop-obj"
+      mtl="#shop-mtl"
+      scale="0.05 0.05 0.05"
+    ></a-obj-model>
+    <a-sky color="#000"></a-sky>
+  </a-scene>
 </template>
 
 <script lang="ts">
