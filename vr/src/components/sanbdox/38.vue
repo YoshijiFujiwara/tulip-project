ba92801a17bf630fd6061149687e13772cf3a3af
YoshijiFujiwara <yfbiology2@gmail.com>
2020-12-01 15:16:43 +0900

    fix prettier problem

--- vr/src/components/sanbdox/38.vue
+++ vr/src/components/sanbdox/38.vue
@@ -50,69 +50,69 @@
 <script>
 export default {
   mounted() {
-    window.AFRAME.registerComponent('multiple-entities', {
+    window.AFRAME.registerComponent("multiple-entities", {
       schema: {
-        num: { type: 'number', default: 5 },
-        w: { type: 'number', default: 1 },
-        h: { type: 'number', default: 1 },
-        d: { type: 'number', default: 1 },
-        colorDefault: { type: 'color', default: '#212121' },
-        colorHover: { type: 'color', default: '#FF7700' },
-        axis: { type: 'string', default: 'x' },
-        dir: { type: 'string', default: 'positive' },
-        gap: { type: 'number', default: 0 }
+        num: { type: "number", default: 5 },
+        w: { type: "number", default: 1 },
+        h: { type: "number", default: 1 },
+        d: { type: "number", default: 1 },
+        colorDefault: { type: "color", default: "#212121" },
+        colorHover: { type: "color", default: "#FF7700" },
+        axis: { type: "string", default: "x" },
+        dir: { type: "string", default: "positive" },
+        gap: { type: "number", default: 0 },
       },
 
       init() {
-        const el = this.el
-        const data = this.data
+        const el = this.el;
+        const data = this.data;
 
         for (let i = 0; i < data.num; i++) {
-          const myBox = document.createElement('a-entity')
+          const myBox = document.createElement("a-entity");
 
-          myBox.setAttribute('geometry', {
-            primitive: 'box',
+          myBox.setAttribute("geometry", {
+            primitive: "box",
             width: data.w,
             height: data.h,
-            depth: data.d
-          })
+            depth: data.d,
+          });
 
-          myBox.setAttribute('material', 'color', data.colorDefault)
+          myBox.setAttribute("material", "color", data.colorDefault);
 
-          const alignX = i * (data.w + data.gap) + data.w / 2
-          const alignY = i * (data.h + data.gap) + data.h / 2
-          const alignZ = i * (data.d + data.gap)
+          const alignX = i * (data.w + data.gap) + data.w / 2;
+          const alignY = i * (data.h + data.gap) + data.h / 2;
+          const alignZ = i * (data.d + data.gap);
 
-          let direction
+          let direction;
 
-          if (data.dir === 'positive') {
-            direction = 1
-          } else if (data.dir === 'negative') {
-            direction = -1
+          if (data.dir === "positive") {
+            direction = 1;
+          } else if (data.dir === "negative") {
+            direction = -1;
           }
 
-          if (data.axis === 'x') {
-            myBox.object3D.position.x = alignX * direction
-            myBox.object3D.position.y = data.h / 2
-          } else if (data.axis === 'y') {
-            myBox.object3D.position.y = alignY * direction
-          } else if (data.axis === 'z') {
-            myBox.object3D.position.z = alignZ * direction
-            myBox.object3D.position.y = data.h / 2
+          if (data.axis === "x") {
+            myBox.object3D.position.x = alignX * direction;
+            myBox.object3D.position.y = data.h / 2;
+          } else if (data.axis === "y") {
+            myBox.object3D.position.y = alignY * direction;
+          } else if (data.axis === "z") {
+            myBox.object3D.position.z = alignZ * direction;
+            myBox.object3D.position.y = data.h / 2;
           }
 
-          el.appendChild(myBox)
+          el.appendChild(myBox);
 
-          myBox.addEventListener('mouseenter', function () {
-            myBox.setAttribute('material', 'color', data.colorHover)
-          })
+          myBox.addEventListener("mouseenter", function () {
+            myBox.setAttribute("material", "color", data.colorHover);
+          });
 
-          myBox.addEventListener('mouseleave', function () {
-            myBox.setAttribute('material', 'color', data.colorDefault)
-          })
+          myBox.addEventListener("mouseleave", function () {
+            myBox.setAttribute("material", "color", data.colorDefault);
+          });
         }
-      }
-    })
-  }
-}
+      },
+    });
+  },
+};
 </script>
