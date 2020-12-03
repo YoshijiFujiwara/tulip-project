bb8882e8d06e66bb8cf5efaac13b3f17fcb7e705
YoshijiFujiwara <yfbiology2@gmail.com>
2020-12-04 02:38:55 +0900

    うごきましたよ

--- react-vr/src/index.js
+++ react-vr/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'aframe';
-import 'networked-aframe';
-import 'socket.io';
+// import 'aframe';
+// import 'networked-aframe';
+// import 'aframe-randomizer-components';
+// import 'socket.io';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Global vars and functions
+// import './NAF/NafIndex';
+// Network components
+// import './NAF/components/networked-scene';
+// import './NAF/components/networked';
+// import './NAF/components/networked-audio-source';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
--- react-vr/src/pages/NetWorkedSample.jsx
+++ react-vr/src/pages/NetWorkedSample.jsx
@@ -1,20 +1,146 @@
+// import 'aframe-randomizer-components';
+// import 'aframe-particle-system-component';
 import { Scene, Entity } from 'aframe-react';
+import { useEffect } from 'react';
 
 export default function NetWorkedSample() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    // script.async = true;
+    script.innerHTML = `
+    var isMobile = AFRAME.utils.device.isMobile();
+  
+      if (isMobile) {
+        var particles = document.getElementById('particles');
+        particles.parentNode.removeChild(particles);
+      }
+    `;
+    document.body.appendChild(script);
+
+    const script2 = document.createElement('script');
+    script2.type = 'text/javascript';
+    // script2.async = true;
+    script2.innerHTML = `
+    // Define custom schema for syncing avatar color, set by random-color
+      NAF.schemas.add({
+        template: '#avatar-template',
+        components: [
+          'position',
+          'rotation',
+          {
+            selector: '.head',
+            component: 'material',
+            property: 'color'
+          }
+        ]
+      });
+  
+      // Called by Networked-Aframe when connected to server
+      function onConnect () {
+        console.log("onConnect", new Date());
+      }
+    `;
+    document.body.appendChild(script2);
+
+    return () => {
+      document.body.removeChild(script);
+      document.body.removeChild(script2);
+    };
+  }, []);
+
   return (
-    <a-scene networked-scene>
+    <a-scene
+      networked-scene={{
+        room: 'basic-audio',
+        debug: true,
+        adapter: 'webrtc',
+        audio: true,
+      }}
+    >
       <a-assets>
+        <img
+          id="grid"
+          src="https://img.gs/bbdkhfbzkk/stretch/https://i.imgur.com/25P1geh.png"
+          crossorigin="anonymous"
+          alt=""
+        />
+        <img
+          id="sky"
+          src="https://i.imgur.com/WqlqEkq.jpg"
+          crossorigin="anonymous"
+          alt=""
+        />
+
         <template id="avatar-template">
-          <Entity primitive="a-sphere"></Entity>
+          <a-entity class="avatar" networked-audio-source>
+            <a-sphere
+              class="head"
+              color="#ffffff"
+              scale="0.45 0.5 0.4"
+            ></a-sphere>
+            <a-entity class="face" position="0 0.05 0">
+              <a-sphere
+                class="eye"
+                color="#efefef"
+                position="0.16 0.1 -0.35"
+                scale="0.12 0.12 0.12"
+              >
+                <a-sphere
+                  class="pupil"
+                  color="#000"
+                  position="0 0 -1"
+                  scale="0.2 0.2 0.2"
+                ></a-sphere>
+              </a-sphere>
+              <a-sphere
+                class="eye"
+                color="#efefef"
+                position="-0.16 0.1 -0.35"
+                scale="0.12 0.12 0.12"
+              >
+                <a-sphere
+                  class="pupil"
+                  color="#000"
+                  position="0 0 -1"
+                  scale="0.2 0.2 0.2"
+                ></a-sphere>
+              </a-sphere>
+            </a-entity>
+          </a-entity>
         </template>
       </a-assets>
-      <Entity
+
+      <a-entity
         id="player"
         networked="template:#avatar-template;attachTemplateToLocal:false;"
         camera
+        position="0 1.6 0"
+        spawn-in-circle="radius:3"
         wasd-controls
         look-controls
-      ></Entity>
+      >
+        <a-sphere class="head" visible="false" random-color></a-sphere>
+      </a-entity>
+
+      <a-entity
+        position="0 0 0"
+        geometry="primitive: plane; width: 10000; height: 10000;"
+        rotation="-90 0 0"
+        material="src: #grid; repeat: 10000 10000; transparent: true; metalness:0.6; roughness: 0.4; sphericalEnvMap: #sky;"
+      ></a-entity>
+
+      <a-entity
+        light="color: #ccccff; intensity: 1; type: ambient;"
+        visible=""
+      ></a-entity>
+      <a-entity
+        light="color: #ffaaff; intensity: 1.5"
+        position="5 5 5"
+      ></a-entity>
+
+      <a-sky src="#sky" rotation="0 -90 0"></a-sky>
+      <a-entity id="particles" particle-system="preset: snow"></a-entity>
     </a-scene>
   );
 }
