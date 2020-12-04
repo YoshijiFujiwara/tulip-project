6b7a5bdc60dc198f56a8e8f49d9e19a64a87a244
Sui1224 <sui.ohs75003@gmail.com>
2020-12-04 11:38:20 +0900

    ドロワーメニュー以外ほぼできた

--- frontend/layouts/admin.vue
+++ frontend/layouts/admin.vue
@@ -6,59 +6,35 @@
       :clipped="clipped"
       fixed
       app
+      color="#27303d"
     >
       <v-list>
+      <v-list-item>
+        <v-img :src="require('@/assets/logo_main.png')" />
+      </v-list-item>
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
           :to="item.to"
           router
+          link
           exact
         >
           <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
+            <v-icon color="white">{{ item.icon }}</v-icon>
           </v-list-item-action>
-          <v-list-item-content>
+          <v-list-item-content class="white--text">
             <v-list-item-title v-text="item.title" />
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+    
     <v-main>
       <v-container>
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
@@ -71,19 +47,19 @@ export default {
       fixed: false,
       items: [
         {
-          icon: 'mdi-apps',
+          icon: 'mdi-home',
           title: 'ダッシュボード',
         },
         {
-          icon: 'mdi-chart-bubble',
+          icon: 'mdi-shield-account-variant',
           title: '管理者',
         },
         {
-          icon: 'mdi-apps',
+          icon: 'mdi-contacts',
           title: '出展者',
         },
         {
-          icon: 'mdi-chart-bubble',
+          icon: 'mdi-account-supervisor',
           title: '来場者',
         },
       ],
--- frontend/pages/admin/index.vue
+++ frontend/pages/admin/index.vue
@@ -1,45 +1,16 @@
 <template>
   <v-container fluid grid-list-lg light>
-    <v-layout row wrap>
-      <v-flex d-flex xs6 md4>
-        <v-card color="purple" dark>
-          <v-card-title primary class="title">Lorem</v-card-title>
-          <v-card-text>hogehuga</v-card-text>
+    <v-layout row wrap class="mt-6">
+      <v-flex d-flex xs6 md5>
+        <v-card color="#b7b7b7" dark width=300 height=300 hover>
+          <v-card-title primary class="title justify-center" ><v-icon size=200>mdi-account-supervisor</v-icon></v-card-title>
+          <v-card-text class="text-center text-h5">展示者情報</v-card-text>
         </v-card>
       </v-flex>
-      <v-flex d-flex xs6 md4>
-        <v-card color="blue" dark>
-          <v-card-title primary class="title">Lorem</v-card-title>
-          <v-card-text>hogehuga</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex d-flex xs6 md4>
-        <v-card color="pink" dark>
-          <v-card-title primary class="title">Lorem</v-card-title>
-          <v-card-text>hogehuga</v-card-text>
-        </v-card>
-      </v-flex>
-      <v-flex d-flex xs6 md4>
-        <v-card color="orange" dark>
-          <v-card-title primary class="title">Lorem</v-card-title>
-          <v-card-text>hogehuga</v-card-text>
-        </v-card>
-      </v-flex>
-    </v-layout>
-    <v-layout>
-      <v-flex text-xs-center>
-        <v-card>
-          <v-card-text>
-            <img src="/v.png" alt="Vuetify.js" class="mb-5" />
-            <blockquote class="blockquote">
-              &#8220;First, solve the problem. Then, write the code.&#8221;
-              <footer>
-                <small>
-                  <em>&mdash;John Johnson</em>
-                </small>
-              </footer>
-            </blockquote>
-          </v-card-text>
+      <v-flex d-flex xs6 md5>
+        <v-card color="#b7b7b7" dark width=300 height=300 hover>
+          <v-card-title primary class="title justify-center"><v-icon size=200>mdi-heart</v-icon></v-card-title>
+          <v-card-text class="text-center text-h5">出席確認</v-card-text>
         </v-card>
       </v-flex>
     </v-layout>
