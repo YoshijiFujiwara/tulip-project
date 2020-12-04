22fa208ab157ee3c57d0d9313f27a48be38d8864
kooririri <piaoxianglunb@gmail.com>
2020-12-04 11:15:20 +0900

    ブース登録画面を、zeplinに沿って作成

--- frontend/components/exhibitors/mypage/UploadBoothDialog.vue
+++ frontend/components/exhibitors/mypage/UploadBoothDialog.vue
@@ -1,13 +1,39 @@
 <template>
   <v-dialog v-model="dialog" max-width="700px">
-    <v-card> あああ </v-card>
+    <v-card>
+      <v-card-title class="display-1 font-weight-bold pt-8">
+        ブース登録
+      </v-card-title>
+      <v-card-subtitle class="mt-2">展示場所の選択</v-card-subtitle>
+      <v-form ref="form" v-model="valid" class="ml-8 mr-8 pb-9">
+        <v-img :src="require('@/assets/stage.png')"></v-img>
+        <v-select
+          v-model="form.booth"
+          class="pb-3"
+          :rules="rules.booth"
+          :items="items"
+          label="ブース番号"
+          placeholder="選択"
+          color="deep-purple darken-4"
+          outlined
+        ></v-select>
+        <v-btn
+          block
+          large
+          class="white--text"
+          color="deep-purple darken-4"
+          :disabled="!valid || isLoading"
+          @click="onSubmit"
+        >
+          ブースを登録する
+        </v-btn>
+      </v-form>
+    </v-card>
   </v-dialog>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from 'nuxt-property-decorator'
-// cloudinaryに画像をアップロードする関数は、このファイル限定で使用するとは限らないため、別の場所に切り出した
-import { uploadImageCloudinary } from '../../../utils/functions'
 import ExhibitApi from '../../../plugins/axios/modules/exhibit'
 import ProfileApi from '../../../plugins/axios/modules/profile'
 import { Exhibit } from '../../../types/exhibit'
@@ -19,149 +45,20 @@ export default class CreateExhibitDialog extends Vue {
 
   isLoading: boolean = false // ローディング判定
 
-  items = ['game', 'music', 'movie', 'it']
+  items = ['01', '02', '03', '04']
   valid = false
-  uploadThumbnailImageUrl = ''
-  uploadPresentationImageUrl = ''
-  exhibitId: number | null = null // 作品の更新時に用いるID
 
   form = {
-    title: '',
-    description: '',
-    genre: '',
-    thumbnailImage: (null as unknown) as File | null,
-    presentationImage: (null as unknown) as File | null,
+    booth: '',
   }
 
   rules = {
-    title: [(v: string) => !!v || 'タイトルは必須です'],
-    description: [(v: string) => !!v || '説明文は必須です'],
-    genre: [(v: string) => !!v || 'ジャンルは必須です'],
-    thumbnailImage: [(v: string) => !!v || 'サムネイル画像は必須です'],
-    presentationImage: [(v: string) => !!v || 'プレゼンデータ画像は必須です'],
-  }
-
-  // exhibitIdが入っていれば、更新モードとする
-  get isUpdateMode() {
-    return !!this.exhibitId
-  }
-
-  created() {
-    // 自分が登録している作品情報を取得する
-    ProfileApi.getProfileExhibits()
-      .then((exhibit: Exhibit) => {
-        this.form.title = exhibit.title
-        this.form.description = exhibit.description
-        this.form.genre = exhibit.genre
-        this.uploadThumbnailImageUrl = exhibit.thumbnail
-        this.uploadPresentationImageUrl = exhibit.presentationImage
-        this.exhibitId = exhibit.id
-      })
-      .catch(() => {
-        this.$toast.error('作品登録の際にエラーが発生しました')
-        this.dialog = false
-      })
+    booth: [(v: string) => !!v || 'ブースは必須です'],
   }
 
   async onSubmit() {
     this.isLoading = true
-
-    if (this.isUpdateMode) {
-      // cloudinaryにサムネイルとプレゼン画像のアップロードをする
-      // api側には、cloudinaryから返却されたimageのurlを渡す形となる
-      let thumbnailImageUrl: string | null = null
-      let presentationImageUrl: string | null = null
-
-      if (this.form.thumbnailImage) {
-        thumbnailImageUrl = await uploadImageCloudinary(
-          this.$axios,
-          this.form.thumbnailImage
-        )
-      }
-      if (this.form.presentationImage) {
-        presentationImageUrl = await uploadImageCloudinary(
-          this.$axios,
-          this.form.presentationImage
-        )
-      }
-
-      ExhibitApi.updateExhibit(this.exhibitId!, {
-        ...this.form,
-        thumbnail: thumbnailImageUrl || this.uploadThumbnailImageUrl,
-        presentationImage:
-          presentationImageUrl || this.uploadPresentationImageUrl,
-      })
-        .then(() => {
-          this.$toast.success('作品を更新しました')
-        })
-        .catch(() => {
-          this.$toast.error('作品更新の際にエラーが発生しました')
-        })
-    } else {
-      // cloudinaryにサムネイルとプレゼン画像のアップロードをする
-      // api側には、cloudinaryから返却されたimageのurlを渡す形となる
-      const thumbnailImageUrl = await uploadImageCloudinary(
-        this.$axios,
-        this.form.thumbnailImage!
-      )
-      const presentationImageUrl = await uploadImageCloudinary(
-        this.$axios,
-        this.form.presentationImage!
-      )
-      ExhibitApi.createExhibit({
-        ...this.form,
-        thumbnail: thumbnailImageUrl,
-        presentationImage: presentationImageUrl,
-      })
-        .then((res) => {
-          this.exhibitId = res.id
-          this.form.thumbnailImage = null
-          this.form.presentationImage = null
-          this.$toast.success('作品を登録しました')
-        })
-        .catch(() => {
-          this.$toast.error('作品登録の際にエラーが発生しました')
-        })
-    }
-
-    this.dialog = false
-    this.isLoading = false
-  }
-
-  // thumbnailImageのプレビュー
-  onThumbnailImagePicked(file: File) {
-    if (file !== undefined && file !== null) {
-      if (file.name.lastIndexOf('.') <= 0) {
-        return
-      }
-      const fr = new FileReader()
-      fr.readAsDataURL(file)
-      fr.addEventListener('load', () => {
-        if (typeof fr.result === 'string') {
-          this.uploadThumbnailImageUrl = fr.result
-        }
-      })
-    } else {
-      this.uploadThumbnailImageUrl = ''
-    }
-  }
-
-  // presentationImageのプレビュー
-  onPresentationImagePicked(file: File) {
-    if (file !== undefined && file !== null) {
-      if (file.name.lastIndexOf('.') <= 0) {
-        return
-      }
-      const fr = new FileReader()
-      fr.readAsDataURL(file)
-      fr.addEventListener('load', () => {
-        if (typeof fr.result === 'string') {
-          this.uploadPresentationImageUrl = fr.result
-        }
-      })
-    } else {
-      this.uploadPresentationImageUrl = ''
-    }
+    alert('ブース登録しましたはず')
   }
 
   // モーダルの開閉のために必要
