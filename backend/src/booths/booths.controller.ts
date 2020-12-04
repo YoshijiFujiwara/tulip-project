98ab87f6eacde19bb1b8060e6008adf77fc06e7a
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-12-04 10:53:31 +0900

    update: DI booth service -> controller

--- backend/src/booths/booths.controller.ts
+++ backend/src/booths/booths.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { BoothsService } from './booths.service';
 
+@ApiTags('booths')
 @Controller('booths')
-export class BoothsController {}
+export class BoothsController {
+  constructor(private boothsService: BoothsService) {}
+}
