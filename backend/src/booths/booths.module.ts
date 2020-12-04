d4a3850dfb8462ef44fa141836597fb030e10d2e
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-12-04 11:03:47 +0900

    update: DI groupRepository -> boothsService

--- backend/src/booths/booths.module.ts
+++ backend/src/booths/booths.module.ts
@@ -3,9 +3,10 @@ import { BoothsController } from './booths.controller';
 import { BoothsService } from './booths.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoothRepository } from '../entities/booth.repository';
+import { GroupRepository } from '../entities/group.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([BoothRepository])],
+  imports: [TypeOrmModule.forFeature([BoothRepository, GroupRepository])],
   controllers: [BoothsController],
   providers: [BoothsService],
 })
--- backend/src/booths/booths.service.ts
+++ backend/src/booths/booths.service.ts
@@ -1,11 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BoothRepository } from '../entities/booth.repository';
+import { CreateBoothDto } from './dto/create-booth.dto';
+import { ExhibitorEntity } from '../entities/exhibitor.entity';
+import { GroupRepository } from '../entities/group.repository';
 
 @Injectable()
 export class BoothsService {
   constructor(
     @InjectRepository(BoothRepository)
     private boothRepository: BoothRepository,
+    @InjectRepository(GroupRepository)
+    private groupRepository: GroupRepository,
   ) {}
+
+  async createBooth(
+    createBoothDto: CreateBoothDto,
+    exhibitor: ExhibitorEntity,
+  ) {
+    // 登録番号が重複していないかチェック
+  }
 }
