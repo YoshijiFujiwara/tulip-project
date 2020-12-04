e79ccd139d4183db7b2c1f937542db079435444e
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-12-04 10:53:17 +0900

    update: DI booth repository -> service

--- backend/src/booths/booths.service.ts
+++ backend/src/booths/booths.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { BoothRepository } from '../entities/booth.repository';
 
 @Injectable()
-export class BoothsService {}
+export class BoothsService {
+  constructor(
+    @InjectRepository(BoothRepository)
+    private boothRepository: BoothRepository,
+  ) {}
+}
