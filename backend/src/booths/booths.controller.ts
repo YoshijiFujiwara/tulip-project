535c158198a482eff2335554a93645dd80f8b369
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-12-04 11:01:17 +0900

    add: create createBooth()

--- backend/src/booths/booths.controller.ts
+++ backend/src/booths/booths.controller.ts
@@ -1,9 +1,27 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { BoothsService } from './booths.service';
+import { AuthGuard } from '@nestjs/passport';
+import { CreateBoothDto } from './dto/create-booth.dto';
+import { GetUser } from '../auth/get-user-decorator';
+import { ExhibitorEntity } from '../entities/exhibitor.entity';
 
 @ApiTags('booths')
 @Controller('booths')
 export class BoothsController {
   constructor(private boothsService: BoothsService) {}
+
+  @Post()
+  @UseGuards(AuthGuard('jwt'))
+  @ApiBearerAuth()
+  async createBooth(
+    @Body(ValidationPipe) createBoothDto: CreateBoothDto,
+    @GetUser() exhibitor: ExhibitorEntity,
+  ) {}
 }
