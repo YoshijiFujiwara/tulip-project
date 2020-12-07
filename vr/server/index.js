b6ac8bd5332122e6718737a94fbf92d3d5237c28
YoshijiFujiwara <yfbiology2@gmail.com>
2020-12-07 12:09:52 +0900

    setting eslint

--- vr/server/index.js
+++ vr/server/index.js
@@ -1,53 +1,53 @@
 // Load required modules
-const https = require("https");
-const path = require("path");
-const express = require("express"); // web framework external module
-const fs = require("fs");
+const https = require('https');
+const path = require('path');
+const express = require('express'); // web framework external module
+const fs = require('fs');
 
 const privateKey = fs.readFileSync(__dirname + '/localhost-key.pem', 'utf-8');
 const certificate = fs.readFileSync(__dirname + '/localhost.pem', 'utf-8');
 const credentials = {
   key: privateKey,
-  cert: certificate
+  cert: certificate,
 };
 
 // Set process name
-process.title = "networked-aframe-server";
+process.title = 'networked-aframe-server';
 
 // Get port or default to 8080
 const port = process.env.PORT || 8080;
 
 // Setup and configure Express http server.
 const app = express();
-app.use(express.static(path.resolve(__dirname, "..", "pages")));
+app.use(express.static(path.resolve(__dirname, '..', 'pages')));
 
 console.log('env', process.env.NODE_ENV);
 
 // Serve the example and build the bundle in development.
-if (process.env.NODE_ENV === "development") {
-  const webpackMiddleware = require("webpack-dev-middleware");
-  const webpack = require("webpack");
-  const config = require("../webpack.dev");
+if (process.env.NODE_ENV === 'development') {
+  const webpackMiddleware = require('webpack-dev-middleware');
+  const webpack = require('webpack');
+  const config = require('../webpack.dev');
 
   app.use(
     webpackMiddleware(webpack(config), {
-      publicPath: "/dist/"
-    })
+      publicPath: '/dist/',
+    }),
   );
 }
 
 // Start Express http server
 const webServer = https.createServer(credentials, app);
-const io = require("socket.io")(webServer);
+const io = require('socket.io')(webServer);
 
 const rooms = {};
 
-io.on("connection", socket => {
-  console.log("user connected", socket.id);
+io.on('connection', (socket) => {
+  console.log('user connected', socket.id);
 
   let curRoom = null;
 
-  socket.on("joinRoom", data => {
+  socket.on('joinRoom', (data) => {
     const { room } = data;
 
     if (!rooms[room]) {
@@ -64,30 +64,30 @@ io.on("connection", socket => {
     console.log(`${socket.id} joined room ${room}`);
     socket.join(room);
 
-    socket.emit("connectSuccess", { joinedTime });
+    socket.emit('connectSuccess', { joinedTime });
     const occupants = rooms[room].occupants;
-    io.in(curRoom).emit("occupantsChanged", { occupants });
+    io.in(curRoom).emit('occupantsChanged', { occupants });
   });
 
-  socket.on("send", data => {
-    io.to(data.to).emit("send", data);
+  socket.on('send', (data) => {
+    io.to(data.to).emit('send', data);
   });
 
-  socket.on("broadcast", data => {
-    socket.to(curRoom).broadcast.emit("broadcast", data);
+  socket.on('broadcast', (data) => {
+    socket.to(curRoom).broadcast.emit('broadcast', data);
   });
 
-  socket.on("disconnect", () => {
+  socket.on('disconnect', () => {
     console.log('disconnected: ', socket.id, curRoom);
     if (rooms[curRoom]) {
-      console.log("user disconnected", socket.id);
+      console.log('user disconnected', socket.id);
 
       delete rooms[curRoom].occupants[socket.id];
       const occupants = rooms[curRoom].occupants;
-      socket.to(curRoom).broadcast.emit("occupantsChanged", { occupants });
+      socket.to(curRoom).broadcast.emit('occupantsChanged', { occupants });
 
       if (occupants == {}) {
-        console.log("everybody left room");
+        console.log('everybody left room');
         delete rooms[curRoom];
       }
     }
@@ -95,5 +95,5 @@ io.on("connection", socket => {
 });
 
 webServer.listen(port, () => {
-  console.log("listening on http://localhost:" + port);
+  console.log('listening on http://localhost:' + port);
 });
