7b8e2c29eef0a8ce5ab8a137a23da9a618697232
YoshijiFujiwara <yfbiology2@gmail.com>
2020-12-07 10:30:13 +0900

    出来なかったので、削除:cry:

--- react-vr/src/aframe-helper/refresh-obj.js
+++
@@ -1,8 +0,0 @@
-window.AFRAME.registerComponent('refresh-obj', {
-  init() {
-    this.el.addEventListener('click', function () {
-      const myGBcursor = document.querySelector('#myGBcursor');
-      myGBcursor.components.raycaster.refreshObjects();
-    });
-  },
-});
--- react-vr/src/pages/Index.jsx
+++
@@ -1,3 +0,0 @@
-export default function Index() {
-  return <div>ここに本番のVR画面がくるやで</div>;
-}
