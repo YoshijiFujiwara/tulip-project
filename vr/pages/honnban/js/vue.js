787b681134718563f050ca9c8988c03e8f3eda59
YoshijiFujiwara <yfbiology2@gmail.com>
2020-12-07 15:49:15 +0900

    api通信出来るように

---
+++ vr/pages/honnban/js/vue.js
@@ -0,0 +1,17 @@
+// when life is settled, load up the fun stuff
+document.addEventListener('DOMContentLoaded', function () {
+  // eslint-disable-next-line no-undef
+  new Vue({
+    el: '#vue-root',
+    data: {
+      exhibits: [],
+    },
+    created() {
+      // eslint-disable-next-line no-undef
+      axios.get(`https://localhost:3001/api/exhibits`).then((res) => {
+        const exhibits = res.data;
+        this.exhibits = exhibits;
+      });
+    },
+  });
+});
