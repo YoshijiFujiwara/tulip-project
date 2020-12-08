6e633212fd5a310e6649cb040839a7996d190f7d
Sui1224 <sui.ohs75003@gmail.com>
2020-12-08 18:30:21 +0900

    コンソールログ消した

--- frontend/pages/admin/exhibits.vue
+++ frontend/pages/admin/exhibits.vue
@@ -1,30 +1,33 @@
 <template>
-  <v-simple-table>
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-left">id</th>
-          <th class="text-left">groupId</th>
-          <th class="text-left">title</th>
-          <th class="text-left">genre</th>
-          <th class="text-left">description</th>
-          <th class="text-left">presentationImage</th>
-          <th class="text-left">thumbnail</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="exhibit in exhibits" :key="exhibit.id">
-          <td>{{ exhibit.id }}</td>
-          <td>{{ exhibit.groupId }}</td>
-          <td>{{ exhibit.title }}</td>
-          <td>{{ exhibit.genre }}</td>
-          <td>{{ exhibit.description }}</td>
-          <td>{{ exhibit.presentationImage }}</td>
-          <td>{{ exhibit.thumbnail }}</td>
-        </tr>
-      </tbody>
-    </template>
-  </v-simple-table>
+  <div>
+    <h1>作品一覧ページです</h1>
+    <v-simple-table>
+      <template v-slot:default>
+        <thead>
+          <tr>
+            <th class="text-left">id</th>
+            <th class="text-left">groupId</th>
+            <th class="text-left">title</th>
+            <th class="text-left">genre</th>
+            <th class="text-left">description</th>
+            <th class="text-left">presentationImage</th>
+            <th class="text-left">thumbnail</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="exhibit in exhibits" :key="exhibit.id">
+            <td>{{ exhibit.id }}</td>
+            <td>{{ exhibit.groupId }}</td>
+            <td>{{ exhibit.title }}</td>
+            <td>{{ exhibit.genre }}</td>
+            <td>{{ exhibit.description }}</td>
+            <td>{{ exhibit.presentationImage }}</td>
+            <td>{{ exhibit.thumbnail }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-simple-table>
+  </div>
 </template>
 
 <script lang="ts">
@@ -41,7 +44,6 @@ export default class Signin extends Vue {
   async created() {
     const response = await ExhibitApi.getExhibits()
     this.exhibits = response
-    console.log(this.exhibits)
   }
 }
 </script>
