33cfaf727b05ede3ce0df9ead4937bb3c642e767
Sui1224 <sui.ohs75003@gmail.com>
2020-12-08 18:21:52 +0900

    完？

---
+++ frontend/pages/admin/exhibits.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">id</th>
+          <th class="text-left">groupId</th>
+          <th class="text-left">title</th>
+          <th class="text-left">genre</th>
+          <th class="text-left">description</th>
+          <th class="text-left">presentationImage</th>
+          <th class="text-left">thumbnail</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="exhibit in exhibits" :key="exhibit.id">
+          <td>{{ exhibit.id }}</td>
+          <td>{{ exhibit.groupId }}</td>
+          <td>{{ exhibit.title }}</td>
+          <td>{{ exhibit.genre }}</td>
+          <td>{{ exhibit.description }}</td>
+          <td>{{ exhibit.presentationImage }}</td>
+          <td>{{ exhibit.thumbnail }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+import ExhibitApi from '../../plugins/axios/modules/exhibit'
+
+@Component({
+  auth: false,
+  layout: 'admin',
+})
+export default class Signin extends Vue {
+  exhibits: object = {}
+
+  async created() {
+    const response = await ExhibitApi.getExhibits()
+    this.exhibits = response
+    console.log(this.exhibits)
+  }
+}
+</script>
+
+<style lang="scss"></style>
