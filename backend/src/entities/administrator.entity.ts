e3adf6acf551e5a8b5a40fb0e063a7039bf05c4f
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-12-10 12:02:38 +0900

    add: administrator {entity,repository}

---
+++ backend/src/entities/administrator.entity.ts
@@ -0,0 +1,50 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import * as bcrypt from 'bcrypt';
+
+@Entity({
+  name: 'administrator',
+})
+export class AdministratorEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  @ApiProperty()
+  id!: number;
+
+  @Column({
+    length: 25,
+  })
+  @ApiProperty()
+  name!: string;
+
+  @Column({
+    length: 255,
+    transformer: {
+      to: (raw: string) => bcrypt.hashSync(raw, 5),
+      from: (hashed: string) => hashed,
+    },
+  })
+  @ApiProperty()
+  password!: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  @ApiProperty()
+  createdAt!: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  @ApiProperty()
+  updatedAt!: Date;
+}
