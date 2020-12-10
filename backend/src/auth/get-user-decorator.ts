6707ce766c84d6d4836a3545a3385415a4036660
tk-ozawa <ozawa.shotaro0114@gmail.com>
2020-12-10 23:19:11 +0900

    update: @GetUser()の引数を文字列から配列化

--- backend/src/auth/get-user-decorator.ts
+++ backend/src/auth/get-user-decorator.ts
@@ -6,23 +6,30 @@ import {
 import { JwtPayload } from './interface/jwt-payload.interface';
 import { AdministratorEntity } from '../entities/administrator.entity';
 import { ExhibitorEntity } from '../entities/exhibitor.entity';
+import { UserEntityType } from '../entities/type/user.type';
 
 export const GetUser = createParamDecorator(
   (
-    role: JwtPayload['role'] = 'student',
+    roles: JwtPayload['role'][] = ['student'],
     ctx: ExecutionContext,
-  ): ExhibitorEntity | AdministratorEntity => {
+  ): UserEntityType => {
     const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
 
-    const isStudent =
-      role === 'student' && request.user instanceof ExhibitorEntity;
-    const isAdmin =
-      role === 'admin' && request.user instanceof AdministratorEntity;
+    let isStudent: boolean;
+    let isAdmin: boolean;
+    roles.forEach(role => {
+      if (!isStudent) {
+        isStudent = role === 'student' && user instanceof ExhibitorEntity;
+      }
+      if (!isAdmin) {
+        isAdmin = role === 'admin' && user instanceof AdministratorEntity;
+      }
+    });
 
     if (isStudent || isAdmin) {
-      return request.user;
-    } else {
-      throw new UnauthorizedException('このAPIを実行する権限がありません。');
+      return user;
     }
+    throw new UnauthorizedException('このAPIを実行する権限がありません。');
   },
 );
