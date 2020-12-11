d204790c5db1bcf759444a0916e2ecf371070bac
libra219 <15705883+libra219@users.noreply.github.com>
2020-12-11 10:07:53 +0900

    🎨 : Change design
    アドミンデザイン変更

--- frontend/pages/admin/signin.vue
+++ frontend/pages/admin/signin.vue
@@ -1,11 +1,14 @@
 <template>
-  <v-container>
-    <v-img :src="require('@/assets/logo_main.png')" />
-    <v-row>
-      <v-col class="mx-auto" cols="7">
-        <div id="form-wrapper">
-          <div id="title-wrapper" class="py-5">SIGN IN</div>
-          <v-row align="center" justify="center" class="row-wrap" no-gutters>
+  <v-row class="signin" align="center" justify="center">
+    <v-container>
+      <v-img
+        max-width="600"
+        class="mx-auto"
+        :src="require('@/assets/logo_main.png')"
+      />
+      <v-row>
+        <v-col class="mx-auto" cols="7">
+          <v-row justify="center" class="row-wrap" no-gutters>
             <v-col cols="8" class="top" align="center">
               <v-form
                 ref="form"
@@ -16,23 +19,21 @@
                 <v-text-field
                   v-model="form.username"
                   :rules="rules.username"
-                  background-color="rgba(255, 255, 255, 0.16)"
-                  dark
-                  prepend-inner-icon="mdi-account"
-                  label="user name"
+                  background-color="rgba(255, 255, 255, 1)"
+                  label="ユーザー名"
                   outlined
                   required
+                  dense
                 ></v-text-field>
                 <v-text-field
                   v-model="form.password"
                   :rules="rules.password"
-                  background-color="rgba(255, 255, 255, 0.16)"
-                  dark
-                  prepend-inner-icon="mdi-lock"
-                  label="password"
+                  background-color="rgba(255, 255, 255, 1)"
+                  label="パスワード"
                   :type="showPasswordIcon ? 'text' : 'password'"
                   outlined
                   required
+                  dense
                   :append-icon="showPasswordIcon ? 'mdi-eye' : 'mdi-eye-off'"
                   @click:append="showPasswordIcon = !showPasswordIcon"
                 ></v-text-field>
@@ -40,19 +41,28 @@
                   id="signin-button"
                   type="submit"
                   block
-                  large
-                  dark
-                  color="rgba(255, 255, 255, 0.23)"
+                  x-large
+                  color="rgba(119, 105, 209, 1)"
                   :disabled="!valid"
-                  >Log in</v-btn
+                  >管理者ログイン</v-btn
                 >
+                <v-btn
+                  id="button"
+                  type="bottun"
+                  elevation="0"
+                  dark
+                  color="rgba(163, 163, 163, 1)"
+                  class="mt-10"
+                >
+                  パスワードを忘れた場合
+                </v-btn>
               </v-form>
             </v-col>
           </v-row>
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-row>
 </template>
 
 <script lang="ts">
@@ -76,13 +86,6 @@ export default class Signin extends Vue {
 
   showPasswordIcon = false
 
-  mounted() {
-    // parallaxコンポーネントを無理やり、高さを合わせる（汚いやり方だけど、仕方ない）
-    if (document.getElementById('parallax-full-height')) {
-      document.getElementById('parallax-full-height')!.style.height = '100vh'
-    }
-  }
-
   onSubmit() {
     alert('lognin')
   }
@@ -91,16 +94,8 @@ export default class Signin extends Vue {
 <style lang="scss">
 // 色情報 from https://tulipgumi.slack.com/archives/C01D34DJC5P/p1604650600001100
 // それをrgbaに変換してます
-#form-wrapper {
-  background-color: rgba(20, 8, 44, 0.59);
-}
-#title-wrapper {
-  background-color: rgba(255, 255, 255, 0.18);
-  font-family: MeiryoUI;
-  font-size: 52px;
-  font-weight: bold;
-  line-height: 0.69;
-  text-align: center;
-  color: rgba(255, 255, 255, 0.76);
+.signin {
+  background-color: #ebebeb;
+  height: 100vh;
 }
 </style>
