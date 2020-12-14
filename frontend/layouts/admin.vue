80bca6a178d231bbae1534a293b6ac02e7f4140e
kooririri <piaoxianglunb@gmail.com>
2020-12-14 12:57:23 +0900

    groupsのnavスタイル（paddingがある問題）を修正、pageTitleを反映

--- frontend/layouts/admin.vue
+++ frontend/layouts/admin.vue
@@ -25,57 +25,73 @@
     </v-navigation-drawer>
 
     <v-main>
-      <v-container fluid grid-list-lg light px-6>
-        <v-app-bar flat>
-          <v-col cols="2">
-            <v-toolbar-title>page title</v-toolbar-title>
-          </v-col>
-          <v-divider vertical></v-divider>
-          <v-btn icon>
-            <v-icon>mdi-reload</v-icon>
-          </v-btn>
-          <v-spacer></v-spacer>
-          <v-form>
-            <v-text-field
-              label="Search"
-              append-icon="mdi-magnify"
-              class="mt-7"
-            ></v-text-field>
-          </v-form>
-        </v-app-bar>
-        <nuxt />
-      </v-container>
+      <v-app-bar flat>
+        <v-col cols="2">
+          <v-toolbar-title>{{ mainTitle }}</v-toolbar-title>
+        </v-col>
+        <v-divider vertical></v-divider>
+        <v-btn icon>
+          <v-icon>mdi-reload</v-icon>
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-form>
+          <v-text-field
+            label="Search"
+            append-icon="mdi-magnify"
+            class="mt-7"
+          ></v-text-field>
+        </v-form>
+      </v-app-bar>
+      <nuxt />
     </v-main>
   </v-app>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      items: [
-        {
-          icon: 'mdi-home',
-          title: 'ダッシュボード',
-        },
-        {
-          icon: 'mdi-shield-account-variant',
-          title: '管理者',
-        },
-        {
-          icon: 'mdi-contacts',
-          title: '出展者一覧',
-        },
-        {
-          icon: 'mdi-contacts',
-          title: '出席確認',
-        },
-        {
-          icon: 'mdi-account-supervisor',
-          title: '来場者',
-        },
-      ],
-    }
-  },
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Admin extends Vue {
+  items = [
+    {
+      icon: 'mdi-home',
+      url: '/admin',
+      title: 'ダッシュボード',
+    },
+    {
+      icon: 'mdi-shield-account-variant',
+      url: '/admin',
+      title: '管理者',
+    },
+    {
+      icon: 'mdi-contacts',
+      url: '/admin/exhibitors',
+      title: '出展者一覧',
+    },
+    {
+      icon: 'mdi-contacts',
+      url: '/admin/',
+      title: '出席確認',
+    },
+    {
+      icon: 'mdi-account-supervisor',
+      url: '/admin/exhibitors',
+      title: '来場者',
+    },
+    {
+      icon: 'mdi-home',
+      url: '/admin/groups',
+      title: '出展者グループ一覧',
+    },
+  ]
+
+  mainTitle: String = ''
+  created() {
+    this.items.forEach((item) => {
+      if (item.url == this.$route.path) {
+        this.mainTitle = item.title
+      }
+    })
+  }
 }
 </script>
