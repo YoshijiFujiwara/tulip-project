49990ee06c030886ebbc8205a6a3c85fd74b5175
Sui1224 <sui.ohs75003@gmail.com>
2020-12-17 11:09:56 +0900

    ある程度デザインに近づいた色など後回し

--- frontend/pages/admin/exhibits/_id/index.vue
+++ frontend/pages/admin/exhibits/_id/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div style="background-color: #f9f9f9; height: 100vh">
     <v-row>
       <v-col cols="7">
         <v-row>
-          <v-col class="text-h3">titleが入る</v-col>
+          <v-col class="text-h3 font-weight-bold ml-12">titleが入る</v-col>
         </v-row>
         <v-row>
-          <v-col>
+          <v-col class="pa-12">
             <v-carousel v-model="model">
               <v-carousel-item v-for="(color, i) in colors" :key="color">
                 <v-sheet :color="color" height="100%" tile>
@@ -19,9 +19,41 @@
           </v-col>
         </v-row>
       </v-col>
-      <v-col cols="5"
-        >uuuuuueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee</v-col
-      >
+      <v-col cols="5">
+        <v-row justify="center" align-content="center">
+          <v-col cols="10 ">
+            <v-img
+              height="400"
+              width="400"
+              :src="require('@/assets/unnamed.png')"
+            ></v-img>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="text-h4 font-weight-bold"> 作品説明 </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="text-h6">
+            作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る
+            作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る
+            作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る
+            作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る
+            作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る作品説明の説明が入る
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col class="text-h5 font-weight-bold"> 作成グループ </v-col>
+          <v-col>グループ名が入る</v-col>
+        </v-row>
+        <v-row>
+          <v-col class="text-h5 font-weight-bold"> グループリーダー </v-col>
+          <v-col>リーダー名が入る</v-col>
+        </v-row>
+        <v-row>
+          <v-col class="text-h5 font-weight-bold"> ジャンル </v-col>
+          <v-col>ジャンル名が入る</v-col>
+        </v-row>
+      </v-col>
     </v-row>
   </div>
 </template>
