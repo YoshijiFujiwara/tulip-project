077a20f4819ba0a6f038c7cbb8adda8ee51cb0da
orphis <s_yufuyufu@icloud.com>
2020-12-21 12:06:28 +0900

    とりあえず表示できた

---
+++ vr/pages/honnban/js/poly.js
@@ -0,0 +1,19 @@
+var request = new XMLHttpRequest();
+request.open('GET', 'https://poly.googleapis.com/v1/assets/9C-MLNfxaor?key=AIzaSyD6jmj2t-ymITRMRkc1rHXOgYaGv9ZnU0M', true);
+request.responseType = 'json';
+
+request.onload = function () {
+    var data = this.response;
+    console.log(data);
+
+    var gltfUrl = ""
+    data.formats.forEach(element => {
+        if (element.formatType == "GLTF2") {
+            gltfUrl = element.root.url;
+        }
+    });
+
+    document.getElementById("polyModel").setAttribute("gltf-model", gltfUrl);
+};
+
+request.send();
