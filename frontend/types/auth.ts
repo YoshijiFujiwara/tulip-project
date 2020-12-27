58d0a2b9e74b8cf87b4b1dc5a072e71d97a63896
YoshijiFujiwara <yfbiology2@gmail.com>
2020-12-27 22:44:27 +0900

    fix prettier problem

--- frontend/types/auth.ts
+++ frontend/types/auth.ts
@@ -1,8 +1,8 @@
 export type User = {
-    id: string,
-    studentNumber: string,
-    name: string
-    lastLoggedinAt: string,
-    attendedAt: string,
-    groupId: number
-};
+  id: string
+  studentNumber: string
+  name: string
+  lastLoggedinAt: string
+  attendedAt: string
+  groupId: number
+}
