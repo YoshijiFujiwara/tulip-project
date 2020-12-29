f97fca82fb0607713cf2a8690c041e8afda1342b
hsmkyo <42708990+hsmkyo@users.noreply.github.com>
2020-12-29 11:07:37 +0900

    リファクタリング

--- vr/pages/honnban/js/aframe/sakuhin-hover.js
+++ vr/pages/honnban/js/aframe/sakuhin-hover.js
@@ -3,11 +3,11 @@ AFRAME.registerComponent('sakuhin-hover', {
     var sakuhinEl = document.querySelector('#i-sakuhin');
     sakuhinEl.addEventListener('mouseenter', function (evt) {
       sakuhinEl.setAttribute('scale', {x: 0.4, y: 0.4, z: 0.4});
-      sakuhinEl.setAttribute('position', {x: -0.283, y: 0.76181, z: 0});
+      sakuhinEl.setAttribute('position', {x: -0.383, y: 0.887, z: 0.4});
     });
     sakuhinEl.addEventListener('mouseleave', function (evt) {
       sakuhinEl.setAttribute('scale', {x: 0.3, y: 0.3, z: 0.3});
-      sakuhinEl.setAttribute('position', {x: -0.283, y: 0.63654, z: 0});
+      sakuhinEl.setAttribute('position', {x: -0.383, y: 0.8, z: 0.4});
     });
   },
 });
