b3218c328dd5df388cbfcdc12bb177020dd2e5e2
kooririri <piaoxianglunb@gmail.com>
2021-01-04 11:29:54 +0900

    「frontend - frontend 」に、今回のアプリケーション名を反映する

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -46,8 +46,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - frontend',
-    title: 'frontend',
+    title: '文化祭から始まるVR生活',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
