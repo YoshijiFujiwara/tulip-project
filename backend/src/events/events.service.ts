20879add50fcd88ac990281e9f82543e17bb841a
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-01-04 12:29:06 +0900

    update: eventsの各moduleをそれぞれinjectionした

--- backend/src/events/events.service.ts
+++ backend/src/events/events.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EventRepository } from '../entities/event.repository';
 
 @Injectable()
-export class EventsService {}
+export class EventsService {
+  constructor(
+    @InjectRepository(EventRepository)
+    private eventRepository: EventRepository,
+  ) {}
+}
