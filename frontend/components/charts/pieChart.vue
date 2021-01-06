8dcbd4f8301c69b4106fb0924f7ec5f83f9a49e3
kooririri <piaoxianglunb@gmail.com>
2021-01-06 16:01:50 +0900

    ダッシュボードのマシマシデザインを実装する

---
+++ frontend/components/charts/pieChart.vue
@@ -0,0 +1,27 @@
+<script>
+import { Pie } from 'vue-chartjs'
+export default {
+  extends: Pie,
+  data() {
+    return {
+      chartdata: {
+        labels: ['January', 'February'],
+        datasets: [
+          {
+            label: ['Data One'],
+            backgroundColor: '#f87979',
+            data: [40, 30],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartdata, this.options)
+  },
+}
+</script>
--- frontend/pages/admin/index.vue
+++ frontend/pages/admin/index.vue
@@ -1,68 +1,147 @@
 <template>
   <div>
     <breadcrums :items="breadcrum" />
-    <v-layout col wrap class="mt-6">
+    <v-container>
       <!-- ダッシュボードの一列目 -->
-      <v-flex xs12 row>
-        <!-- 展示者情報カード -->
-        <v-flex xs3 class="d-flex justify-center">
-          <v-card color="#b7b7b7" dark width="300" height="300" hover>
-            <v-card-title primary class="title justify-center">
-              <v-icon size="200">mdi-account-supervisor</v-icon>
-            </v-card-title>
-            <v-card-text class="text-center text-h5">展示者情報</v-card-text>
-          </v-card>
-        </v-flex>
-        <!-- 出席確認カード -->
-        <v-flex xs3 class="d-flex justify-center">
-          <v-card color="#b7b7b7" dark width="300" height="300" hover>
-            <v-card-title primary class="title justify-center">
-              <v-icon size="200">mdi-clipboard-check-multiple-outline</v-icon>
-            </v-card-title>
-            <v-card-text class="text-center text-h5">出席確認</v-card-text>
-          </v-card>
-        </v-flex>
-      </v-flex>
-      <v-flex xs12 row class="mt-4 ml-auto">
-        <v-flex xs3 class="d-flex justify-center">
-          <v-card elevation="2" outlined>
-            <!-- <v-card-title>人気作品</v-card-title> -->
-            <v-card-text>
-              <div>人気作品</div>
-              <v-img :src="require('@/assets/chart.jpg')" />
-              <div id="display-change-block" class="text-right mr-4">
-                表示切替
-              </div>
-              <template>
-                <v-tabs right>
-                  <v-tab>いいね数</v-tab>
-                  <v-tab>視聴数</v-tab>
-                </v-tabs>
-              </template>
-              <template>
-                <v-data-table
-                  :headers="headers"
-                  :items="items"
-                  :items-per-page="5"
-                  class="elevation-1"
-                ></v-data-table>
-              </template>
-            </v-card-text>
-          </v-card>
-        </v-flex>
-      </v-flex>
-    </v-layout>
+      <v-row justify="center" align="center">
+        <v-col cols="9">
+          <v-row>
+            <v-card
+              class="px-3 mr-1"
+              max-width="280px"
+              color="#f44336"
+              cols="4"
+            >
+              <v-card-text class="font-white-block">
+                <p class="display-1 font-white-block">出展まで</p>
+                <div class="font-white-block ml-2 mt-5">
+                  <v-row no-gutters>
+                    <v-col cols="4" class="md-2">
+                      <v-icon color="white" x-large>mdi-clock-time-four</v-icon>
+                    </v-col>
+                    <v-col cols="8" class="md-2">
+                      <div>あと</div>
+                      <div style="font-size: 25px">99日 99:99</div>
+                    </v-col>
+                    <v-col class="mt-5">
+                      <v-card-actions>
+                        <v-spacer></v-spacer
+                        ><v-btn rounded outlined color="#ffffff"
+                          >イベント設定</v-btn
+                        >
+                      </v-card-actions>
+                    </v-col>
+                  </v-row>
+                </div>
+              </v-card-text>
+            </v-card>
+            <v-card
+              class="px-3 mr-1"
+              max-width="290px"
+              color="#009688"
+              cols="4"
+            >
+              <v-card-text class="font-white-block">
+                <p class="display-1 font-white-block">出展者数</p>
+                <div class="font-white-block ml-2 mt-5">
+                  <v-row no-gutters>
+                    <v-col cols="4" class="md-2">
+                      <v-icon color="white" x-large>mdi-account</v-icon>
+                    </v-col>
+                    <v-col
+                      cols="8"
+                      class="md-2"
+                      style="font-size: 30px"
+                      align-self="center"
+                    >
+                      99人
+                    </v-col>
+                    <v-col class="mt-5">
+                      <v-card-actions>
+                        <v-spacer></v-spacer
+                        ><v-btn rounded outlined color="#ffffff"
+                          >出展者一覧</v-btn
+                        >
+                      </v-card-actions>
+                    </v-col>
+                  </v-row>
+                </div>
+              </v-card-text>
+            </v-card>
+            <v-card
+              class="px-3 mr-1"
+              max-width="290px"
+              color="#2196f3"
+              cols="4"
+            >
+              <v-card-text class="font-white-block">
+                <p class="display-1 font-white-block">グループ数</p>
+                <div class="font-white-block ml-2 mt-5">
+                  <v-row no-gutters>
+                    <v-col cols="4" class="md-2">
+                      <v-icon color="white" x-large>mdi-account-group</v-icon>
+                    </v-col>
+                    <v-col
+                      cols="8"
+                      class="md-2"
+                      style="font-size: 30px"
+                      align-self="center"
+                    >
+                      9 グループ
+                    </v-col>
+                    <v-col class="mt-5">
+                      <v-card-actions>
+                        <v-spacer></v-spacer
+                        ><v-btn rounded outlined color="#ffffff"
+                          >グループ一覧</v-btn
+                        >
+                      </v-card-actions>
+                    </v-col>
+                  </v-row>
+                </div>
+              </v-card-text>
+            </v-card>
+          </v-row>
+          <v-row justify="center" align="center">
+            <v-col><LineChart /></v-col>
+          </v-row>
+        </v-col>
+        <v-col cols="3">
+          <v-col>
+            <v-card elevation="3" outlined>
+              <!-- <v-card-title>人気作品</v-card-title> -->
+              <v-card-text>
+                <div class="sm_font">人気作品</div>
+                <template>
+                  <v-tabs right>
+                    <v-tab>いいね数</v-tab>
+                    <v-tab>視聴数</v-tab>
+                  </v-tabs>
+                </template>
+                <PieChart />
+                <v-card-actions>
+                  <v-spacer></v-spacer
+                  ><v-btn text color="success">作品一覧</v-btn>
+                </v-card-actions>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 import Breadcrumbs from '../../components/breadcrums.vue'
+import LineChart from '@/components/charts/lineChart.vue'
+import PieChart from '@/components/charts/pieChart.vue'
 
 @Component({
   auth: false,
   layout: 'admin',
-  components: { Breadcrumbs },
+  components: { Breadcrumbs, LineChart, PieChart },
 })
 export default class Signin extends Vue {
   breadcrum = [
@@ -101,6 +180,9 @@ export default class Signin extends Vue {
       number: 6.0,
     },
   ]
+
+  value = [423, 446, 675, 510, 590, 610, 760]
+  labels = ['10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00']
 }
 </script>
 
@@ -108,5 +190,11 @@ export default class Signin extends Vue {
 #display-change-block {
   font-size: 10px;
   margin-bottom: -5px;
+}
+.font-white-block {
+  color: #ffffff;
+}
+.sm_font {
+  font-size: 0.5em;
 }
 </style>
