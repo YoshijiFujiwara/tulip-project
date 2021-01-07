7e62259f61b6ee79d041342bb954cf2aee185c47
YoshijiFujiwara <yfbiology2@gmail.com>
2021-01-07 17:04:11 +0900

    thumbnail追加

--- backend/src/migrations/seeds/providers/exhibits.seed.provider.ts
+++ backend/src/migrations/seeds/providers/exhibits.seed.provider.ts
@@ -29,7 +29,8 @@ export const dummyOriginExhibits: Array<DummyExhibit | null> = [
     genre: GENRE.MUSIC,
     description:
       '重めのリアルロボットアクションゲームのカットシーンを想定し、ストーリーの壮大さと、後半にかけて熱くなる感じが伝わるよう制作しました。サビ前のいかにもなブレイク、わかりやすいメロディーで壮大感と熱いストーリーをイメージしやすく、なおかつロボットでの戦闘を連想しやすいように、機械っぽい音、金属音を要所に入れました。サビの繰り返すときは、パーカッションなどの刻みをやめることで、心に訴えてくる感じになるようにアレンジしました。エピックオーケストラらしい派手で重いミックス、オーケストラ音源のアーティキュレーションの細かいエディットでよりリアルに聴こえるように、曲全体の構成ではわかりやすい展開を意識して、前半は静か、後半で壮大になる2段構えを意識しました。',
-    thumbnail: '',
+    thumbnail:
+      'https://storage.googleapis.com/lab-mode-cms-production/images/Presentation_music_top/original_Presentation_music_top.jpg',
     presentationImages: paging([
       'https://storage.googleapis.com/lab-mode-cms-production/images/Presentation_music_top/original_Presentation_music_top.jpg',
     ]),
@@ -39,7 +40,8 @@ export const dummyOriginExhibits: Array<DummyExhibit | null> = [
     genre: GENRE.MUSIC,
     description:
       '「水」と「青色」をキーワードに、ファンタジーゲームでの水や洞窟のシーンで使われるBGMを想定して制作しました。水をピアノのアルペジオで表現し、テンションノートをたくさん入れることで水面の不安定さを出しました。ピアノのフレーズにベル系のシンセサイザーを重ねたことにより、水の透明感や光の反射による輝きが増したと思います。',
-    thumbnail: '',
+    thumbnail:
+      'https://storage.googleapis.com/lab-mode-cms-production/images/Presentation_music_top/original_Presentation_music_top.jpg',
     presentationImages: paging([
       'https://storage.googleapis.com/lab-mode-cms-production/images/Presentation_music_top/original_Presentation_music_top.jpg',
     ]),
@@ -49,7 +51,8 @@ export const dummyOriginExhibits: Array<DummyExhibit | null> = [
     genre: GENRE.GAME,
     description:
       'このゲームは、☆と☆をつないで星座をつくり、その星座を落としてハム☆スターとともに星のかけらを集めてゆく物理パズルゲームです。落とす星座の形を考える面白さや、物理の動きを利用する面白さを27ステージ楽しめます。絵からステージへ遷移する演出や、背景やハム☆スターの見た目、BGMや星をつなぐ音、各ステージのレベルデザイン等、ゲームの細部にまでこだわって作った完成度の高さがウリです。',
-    thumbnail: '',
+    thumbnail:
+      'https://storage.googleapis.com/lab-mode-cms-production/images/HAL-Game-Hoshi/original_HAL-Game-Hoshi.png',
     presentationImages: paging([
       'https://storage.googleapis.com/lab-mode-cms-production/images/HAL-Game-Hoshi/original_HAL-Game-Hoshi.png',
     ]),
@@ -59,7 +62,8 @@ export const dummyOriginExhibits: Array<DummyExhibit | null> = [
     genre: GENRE.GAME,
     description:
       'DirectX11のHLSLを使ったシェーダーとパーティクルエフェクト。A*アルゴリズムを使った経路探索。',
-    thumbnail: '',
+    thumbnail:
+      'https://storage.googleapis.com/lab-mode-cms-production/images/TH_Presentation_GameProduction_top01/original_TH_Presentation_GameProduction_top01.jpg',
     presentationImages: paging([
       'https://storage.googleapis.com/lab-mode-cms-production/images/TH_Presentation_GameProduction_top01/original_TH_Presentation_GameProduction_top01.jpg',
     ]),
@@ -69,7 +73,8 @@ export const dummyOriginExhibits: Array<DummyExhibit | null> = [
     genre: GENRE.MOVIE,
     description:
       '美しいと思う見せ方や映像表現を追求し、リアリティある映像を目指して制作しました。',
-    thumbnail: '',
+    thumbnail:
+      'https://storage.googleapis.com/lab-mode-cms-production/images/OH_Presentation_CG_top01/original_OH_Presentation_CG_top01.jpg',
     presentationImages: paging([
       'https://storage.googleapis.com/lab-mode-cms-production/images/OH_Presentation_CG_top01/original_OH_Presentation_CG_top01.jpg',
     ]),
@@ -78,7 +83,8 @@ export const dummyOriginExhibits: Array<DummyExhibit | null> = [
     title: 'OASITH',
     genre: GENRE.MOVIE,
     description: '最新の映像表現とリアリティある生物の動きにこだわりました。',
-    thumbnail: '',
+    thumbnail:
+      'https://storage.googleapis.com/lab-mode-cms-production/images/19_oasis_2303/original_19_oasis_2303.png',
     presentationImages: paging([
       'https://storage.googleapis.com/lab-mode-cms-production/images/19_oasis_2303/original_19_oasis_2303.png',
     ]),
@@ -88,7 +94,8 @@ export const dummyOriginExhibits: Array<DummyExhibit | null> = [
     genre: GENRE.IT,
     description:
       '企画・設計に最も時間をかけ、見えないところまでしっかり設定やプロットをつくり込みました。 API化してより使いやすくAWSで実装できているのもアピールポイントです。',
-    thumbnail: '',
+    thumbnail:
+      'https://storage.googleapis.com/lab-mode-cms-production/images/OH_Presentation_Web_top01/original_OH_Presentation_Web_top01.jpg',
     presentationImages: paging([
       'https://storage.googleapis.com/lab-mode-cms-production/images/OH_Presentation_Web_top01/original_OH_Presentation_Web_top01.jpg',
     ]),
@@ -98,7 +105,8 @@ export const dummyOriginExhibits: Array<DummyExhibit | null> = [
     genre: GENRE.IT,
     description:
       '企画・設計に最も時間をかけ、見えないところまでしっかり設定やプロットをつくり込みました。 API化してより使いやすくAWSで実装できているのもアピールポイントです。',
-    thumbnail: '',
+    thumbnail:
+      'https://storage.googleapis.com/lab-mode-cms-production/images/OH_Presentation_Web_top01/original_OH_Presentation_Web_top01.jpg',
     presentationImages: paging([
       'https://storage.googleapis.com/lab-mode-cms-production/images/OH_Presentation_Web_top01/original_OH_Presentation_Web_top01.jpg',
     ]),
