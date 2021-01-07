bf4354669e086d51ffcadda1cc62349e61c7fa16
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-01-07 22:09:52 +0900

    add: アクセスカウントAPI作成&Swagger上の入力欄の最適化

--- backend/src/access-log/access-log.controller.ts
+++ backend/src/access-log/access-log.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, HttpCode, Post, Headers, Query } from '@nestjs/common';
+import { AccessLogService } from './access-log.service';
+import { ApiHeaders, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { CountAccessLogDto } from './dto/count-access-log.dto';
 
-@Controller('access-log')
-export class AccessLogController {}
+@ApiTags('access-log')
+@Controller('access_log')
+export class AccessLogController {
+  constructor(private accessLogService: AccessLogService) {}
+
+  @Post()
+  @HttpCode(201)
+  @ApiOkResponse({
+    description: 'アクセスカウント完了',
+  })
+  @ApiHeaders([
+    {
+      name: 'user-agent',
+      description: 'この入力値は反映されません。',
+      schema: {
+        default:
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+      },
+    },
+    {
+      name: 'referer',
+      description: 'この入力値は反映されません。',
+      schema: {
+        default: 'https://localhost:3001/swagger/',
+      },
+    },
+  ])
+  async countUp(
+    @Headers('user-agent') useragent: string,
+    @Headers('referer') url: string,
+    @Query() countAccessLogDto: CountAccessLogDto,
+  ): Promise<void> {
+    await this.accessLogService.countUp({ useragent, url }, countAccessLogDto);
+  }
+}
