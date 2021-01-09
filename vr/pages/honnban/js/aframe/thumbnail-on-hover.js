06693a0585bac88ae703d1a03addf21291506fbc
ohs70457 <ayuka.70457@gmail.com>
2021-01-09 14:24:16 +0900

    ロビー画面でもクリックできる要素はカーソルホバーでカーソルの色を変える

--- vr/pages/honnban/js/aframe/thumbnail-on-hover.js
+++ vr/pages/honnban/js/aframe/thumbnail-on-hover.js
@@ -1,17 +1,14 @@
 AFRAME.registerComponent('thumbnail-on-hover', {
   init: function () {
-    const aScene = document.getElementsByTagName('a-scene')[0];
-    const exhibits = JSON.parse(aScene.dataset.exhibits)
-    console.log("exhibits", exhibits)
-    exhibits.forEach( (exhibit)=> {
-      const thumbnailEl = document.querySelector(`#exhibit-panel-thumbnail-${exhibit.id}`);
-      const curorEl = document.querySelector('#cursor-item');
+    for(i=1; i< 9; i++) {
+      var thumbnailEl = document.querySelector('#exhibit-panel-thumbnail-'+i);
+      var curorEl = document.querySelector('#cursor-item');
       thumbnailEl.addEventListener('mouseenter', function (evt) {
         curorEl.setAttribute('material', 'color: #00acff');
       });
       thumbnailEl.addEventListener('mouseleave', function (evt) {
         curorEl.setAttribute('material', 'color: #ffffff');
       });
-    })
+    }
   },
 });
