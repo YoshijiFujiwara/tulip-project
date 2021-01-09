a5e0c9e8460faa0546ccfe300ddd03b32708e385
orphis <s_yufuyufu@icloud.com>
2021-01-09 14:13:29 +0900

    ippainaotta

--- frontend/components/charts/lineChart.vue
+++ frontend/components/charts/lineChart.vue
@@ -22,40 +22,44 @@ export default {
         responsive: true,
         maintainAspectRatio: false,
       },
-      accessLog: []
+      accessLog: [],
     }
   },
-  Acceslogs:[],
   async created() {
     const accessLog = await AcceslogApi.getAcceslog()
-    const formatedLog = accessLog.map(log => {
+    const formatedLog = accessLog.map((log) => {
       const dateAccessedAt = this.parseISO(log.accessedAt)
       return {
         ...log,
-        accessedAt: format(dateAccessedAt, 'yyyy年MM月dd日 HH時')
+        accessedAt: format(dateAccessedAt, 'yyyy年MM月dd日 HH時'),
       }
     })
     this.accessLog = formatedLog
     let result = []
-    this.accessLog.forEach(el => {
-      if(result[el.accessedAt]==null){
+    this.accessLog.forEach((el) => {
+      if (result[el.accessedAt] == null) {
         this.chartdata.labels.push(el.accessedAt)
-        result[el.accessedAt]=1
-      }else{
-        result[el.accessedAt]+=1
+        result[el.accessedAt] = 1
+      } else {
+        result[el.accessedAt] += 1
       }
     })
-    this.chartdata.labels.sort(function (a, b) {
-    return ('' + a).localeCompare(b);}).forEach(label => {
-
-        this.chartdata.datasets[0].data = [...this.chartdata.datasets[0].data, result[label]]
+    this.chartdata.labels
+      .sort(function (a, b) {
+        return ('' + a).localeCompare(b)
+      })
+      .forEach((label) => {
+        this.chartdata.datasets[0].data = [
+          ...this.chartdata.datasets[0].data,
+          result[label],
+        ]
       })
   },
-  methods:{
-    parseISO(s) {  
-      var b = s.split(/\D+/);
-      return new Date(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]);
-    }
+  methods: {
+    parseISO(s) {
+      var b = s.split(/\D+/)
+      return new Date(b[0], --b[1], b[2], b[3], b[4], b[5], b[6])
+    },
   },
 
   mounted() {
