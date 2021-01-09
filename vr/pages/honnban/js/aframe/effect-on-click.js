e45788bb3c17492f579c631f386f35e394a3c961
ohs70457 <ayuka.70457@gmail.com>
2021-01-09 11:52:14 +0900

    リアクションパネルをクリックすると効果音を流す

--- vr/pages/honnban/js/aframe/effect-on-click.js
+++ vr/pages/honnban/js/aframe/effect-on-click.js
@@ -5,13 +5,14 @@ AFRAME.registerComponent('effect-on-click', {
       console.log('effect-on-click invoked');
       console.log('event', evt);
       console.log('event target id', evt.target.id);
-
+      var reactionSelectEl = document.querySelector('#'+evt.target.id);
       // angry, cry, love, smile, think, twincle
       // を、idから取り出す
       const reactionType = evt.target.id.replace('i-', '');
-
       // reaction情報をブロードキャストする
       // TODO: 今は動かないので、sakuhin-on-clickの中で仮実装をしています
+      reactionSelectEl.setAttribute('sound', 'src: #assets-'+reactionType+'-effect; volume:2');
+      reactionSelectEl.components.sound.playSound();
     });
   },
 });
