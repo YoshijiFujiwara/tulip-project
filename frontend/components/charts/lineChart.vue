5e3da7687e385b6cf2893f0337dd46a33b0a4437
Sui1224 <sui.ohs75003@gmail.com>
2021-01-09 14:51:39 +0900

    perfect

--- frontend/components/charts/lineChart.vue
+++ frontend/components/charts/lineChart.vue
@@ -1,8 +1,8 @@
 <script>
 import { Line } from 'vue-chartjs'
+import { format } from 'date-fns'
 import AcceslogApi from '../../plugins/axios/modules/getAcceslog'
 import { Acceslog } from '~/types/acceslog'
-import { format } from 'date-fns'
 
 export default {
   extends: Line,
@@ -22,44 +22,49 @@ export default {
         responsive: true,
         maintainAspectRatio: false,
       },
-      accessLog: []
+      accessLog: [],
     }
   },
-  Acceslogs:[],
+  Acceslogs: [],
   async created() {
     const accessLog = await AcceslogApi.getAcceslog()
-    const formatedLog = accessLog.map(log => {
+    const formatedLog = accessLog.map((log) => {
       const dateAccessedAt = this.parseISO(log.accessedAt)
       return {
         ...log,
-        accessedAt: format(dateAccessedAt, 'yyyy年MM月dd日 HH時')
+        accessedAt: format(dateAccessedAt, 'yyyy年MM月dd日 HH時'),
       }
     })
     this.accessLog = formatedLog
-    let result = []
-    this.accessLog.forEach(el => {
-      if(result[el.accessedAt]==null){
+    const result = []
+    this.accessLog.forEach((el) => {
+      if (result[el.accessedAt] == null) {
         this.chartdata.labels.push(el.accessedAt)
-        result[el.accessedAt]=1
-      }else{
-        result[el.accessedAt]+=1
+        result[el.accessedAt] = 1
+      } else {
+        result[el.accessedAt] += 1
       }
     })
-    this.chartdata.labels.sort(function (a, b) {
-    return ('' + a).localeCompare(b);}).forEach(label => {
-
-        this.chartdata.datasets[0].data = [...this.chartdata.datasets[0].data, result[label]]
+    this.chartdata.labels
+      .sort(function (a, b) {
+        return ('' + a).localeCompare(b)
       })
-  },
-  methods:{
-    parseISO(s) {  
-      var b = s.split(/\D+/);
-      return new Date(b[0], --b[1], b[2], b[3], b[4], b[5], b[6]);
-    }
+      .forEach((label) => {
+        this.chartdata.datasets[0].data = [
+          ...this.chartdata.datasets[0].data,
+          result[label],
+        ]
+      })
   },
 
   mounted() {
     this.renderChart(this.chartdata, this.options)
+  },
+  methods: {
+    parseISO(s) {
+      const b = s.split(/\D+/)
+      return new Date(b[0], --b[1], b[2], b[3], b[4], b[5], b[6])
+    },
   },
 }
 </script>
