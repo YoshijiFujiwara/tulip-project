1f15873eb78bb166bb2c198e9437090a914ab95d
orphis <s_yufuyufu@icloud.com>
2021-01-21 13:21:25 +0900

    aa

--- frontend/components/welcome/1stTopImageSection.vue
+++ frontend/components/welcome/1stTopImageSection.vue
@@ -1,14 +1,14 @@
 <template>
   <v-row class justify="center">
     <div class="box">
-    <v-img
-      :lazy-src="require('@/assets/topImage.png')"
-      max-height="720"
-      gradient="to top right, rgba(0,0,0,.33), rgba(0,0,0,.7)"
-      :src="require('@/assets/topImage.png')"
-    ></v-img>
-      <img class="logo" :src="require('@/assets/logo_main.png')">
-  </div>
+      <v-img
+        :lazy-src="require('@/assets/topImage.png')"
+        max-height="720"
+        gradient="to top right, rgba(0,0,0,.33), rgba(0,0,0,.7)"
+        :src="require('@/assets/topImage.png')"
+      ></v-img>
+      <img class="logo" :src="require('@/assets/logo_main.png')" />
+    </div>
   </v-row>
 </template>
 
@@ -19,14 +19,14 @@ import { Component, Vue } from 'nuxt-property-decorator'
 export default class Section extends Vue {}
 </script>
 <style lang="scss">
-.box{
+.box {
   position: relative;
 }
-.logo{
-  height: 124px; 
+.logo {
+  height: 124px;
   width: 389px;
   position: absolute;
   bottom: 0%;
   left: 13%;
-  }
+}
 </style>
