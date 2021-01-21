05fc5c7526942a06d522854201367fc2260bf8ce
tk-ozawa <ozawa.shotaro0114@gmail.com>
2021-01-21 11:51:39 +0900

    update

--- frontend/pages/welcome.vue
+++ frontend/pages/welcome.vue
@@ -1,21 +1,37 @@
 <template>
   <div>
-    <!-- トップの画像のところ -->
-    <FirstTopImageSection />
+    <div class="px-16">
+      <!-- トップの画像のところ -->
+      <FirstTopImageSection />
 
-    <!-- 本サービスについて -->
-    <SecondAboutSection />
+      <!-- 本サービスについて -->
+      <SecondAboutSection />
 
-    <!-- 本サービスの楽しみ方 -->
-    <ThirdOperationManualSection />
+      <!-- 本サービスの楽しみ方 -->
+      <ThirdOperationManualSection />
 
-    <!-- 入場ボタン -->
-    <v-btn>VR空間へ入場</v-btn>
+      <!-- 入場ボタン -->
+      <v-row justify="center" no-gutters>
+        <v-col class="py-12">
+          <v-btn
+            id="enterVR"
+            tile
+            depressed
+            block
+            class="white--text text-center"
+            height="4.5em"
+            x-large
+          >
+            <span>VR空間へ入場</span>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </div>
 
     <!-- フッターをここに -->
     <v-footer color="black" padless>
       <v-row justify="center" no-gutters>
-        <v-col class="black lighten-2 py-4 text-center white--text" cols="12">
+        <v-col class="black py-4 text-center white--text">
           {{ new Date().getFullYear() }} — <span>tulip group</span>
         </v-col>
       </v-row>
@@ -28,6 +44,7 @@ import { Component, Vue } from 'nuxt-property-decorator'
 import FirstTopImageSection from '@/components/welcome/1stTopImageSection.vue'
 import SecondAboutSection from '@/components/welcome/2ndAboutSection.vue'
 import ThirdOperationManualSection from '@/components/welcome/3rdOperationManualSection.vue'
+import colors from 'vuetify/es5/util/colors'
 
 @Component({
   auth: false,
@@ -40,3 +57,13 @@ import ThirdOperationManualSection from '@/components/welcome/3rdOperationManual
 })
 export default class Welcome extends Vue {}
 </script>
+
+<style scoped>
+#enterVR {
+  background-color: #635aab;
+}
+
+#enterVR span {
+  font-size: 1.3em;
+}
+</style>
