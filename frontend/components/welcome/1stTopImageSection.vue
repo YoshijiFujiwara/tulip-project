5f232ce15f233baac0d270c11db6f7c8f2a5e47f
kooririri <piaoxianglunb@gmail.com>
2021-01-22 11:17:55 +0900

    top

--- frontend/components/welcome/1stTopImageSection.vue
+++ frontend/components/welcome/1stTopImageSection.vue
@@ -1,5 +1,19 @@
 <template>
-  <v-row class="" justify="center"> トップの画像がくるやで </v-row>
+  <v-row justify="center">
+    <div class="box">
+      <video
+        :src="require('@/assets/topMovie.mp4')"
+        loop
+        autoplay
+        muted
+        class="mask"
+        style="clip-path: inset(0em 0em 10em 0em)"
+        gradient="to top right, rgba(0,0,0,.33), rgba(0,0,0,.7)"
+        max-height="900px"
+      ></video>
+      <img class="logo" :src="require('@/assets/logo_main.png')" />
+    </div>
+  </v-row>
 </template>
 
 <script lang="ts">
@@ -8,3 +22,16 @@ import { Component, Vue } from 'nuxt-property-decorator'
 @Component({})
 export default class Section extends Vue {}
 </script>
+
+<style lang="scss">
+.box {
+  position: relative;
+}
+.logo {
+  height: 124px;
+  width: 389px;
+  position: absolute;
+  bottom: 20%;
+  left: 10%;
+}
+</style>
