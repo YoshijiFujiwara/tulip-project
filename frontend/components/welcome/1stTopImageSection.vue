77c0aefd4283ca847de881392558b7ed1a55ceb6
Sui1224 <sui.ohs75003@gmail.com>
2021-01-28 13:39:39 +0900

    format

--- frontend/components/welcome/1stTopImageSection.vue
+++ frontend/components/welcome/1stTopImageSection.vue
@@ -1,15 +1,20 @@
 <template>
   <v-row justify="center">
-    <div :class='_isPC ? "boxPC" : "boxSP"'>
-      <video
-        :src="require('@/assets/topMovie.mp4')"
-        loop
-        autoplay
-        muted
-        gradient="to top right, rgba(0,0,0,.33), rgba(0,0,0,.7)"
+    <v-col cols="12">
+      <div :class="_isPC ? 'boxPC' : 'boxSP'">
+        <video
+          :src="require('@/assets/topMovie.mp4')"
+          loop
+          autoplay
+          muted
+          gradient="to top right, rgba(0,0,0,.33), rgba(0,0,0,.7)"
         ></video>
-      <img :class='_isPC ? "logo_PC" : "logo_SP"' :src="require('@/assets/logo_main.png')" />
-    </div>
+        <img
+          :class="_isPC ? 'logo_PC' : 'logo_SP'"
+          :src="require('@/assets/logo_main.png')"
+        />
+      </div>
+    </v-col>
   </v-row>
 </template>
 
