1ea214e769c559fc5c1fc5beacb8fd014f5f0581
YoshijiFujiwara <yfbiology2@gmail.com>
2021-01-29 11:33:07 +0900

    fix prettier

--- frontend/pages/welcome.vue
+++ frontend/pages/welcome.vue
@@ -10,10 +10,18 @@
       <ThirdOperationManualSection :class="_isPC ? 'mt-12' : 'mt-6'" />
     </div>
     <div :class="_isPC ? 'px-16' : ''">
-      <v-row align="center" justify="center" class="text-h6" :class="_isPC ? 'mt-4' : ''">※Chrome推奨</v-row>
+      <v-row
+        align="center"
+        justify="center"
+        class="text-h6"
+        :class="_isPC ? 'mt-4' : ''"
+        >※Chrome推奨</v-row
+      >
       <v-row align="center" justify="center" :class="_isSP ? 'px-6' : ''">
         <v-col align="center">
-          <span class="red--text" :class="_isPC ? 'text-h5' : 'text-h6'">※VR画面では音が出ますので音量にはご注意ください</span>
+          <span class="red--text" :class="_isPC ? 'text-h5' : 'text-h6'"
+            >※VR画面では音が出ますので音量にはご注意ください</span
+          >
         </v-col>
       </v-row>
       <!-- 入場ボタン -->
